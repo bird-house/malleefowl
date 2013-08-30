bdf22d5dd1f5b61f600e2272e0d85e7fd61e5e90
Carsten Ehbrecht <carsten@localhost>
2013-08-31 00:21:39 +0200

    fixed rel_hum process, not using bash script anymore

--- processes/rel_hum_esgf.py
+++ processes/rel_hum_esgf.py
@@ -4,8 +4,10 @@ Author: Nils Hempelmann (nils.hempelmann@hzg)
 """
 
 from datetime import datetime, date
-from malleefowl.process import WorkflowProcess
+import tempfile
 import subprocess
+
+from malleefowl.process import WorkflowProcess
 
 class RelHumProcessEsgf(WorkflowProcess):
     """This process calculates the relative humidity"""
@@ -16,9 +18,6 @@ class RelHumProcessEsgf(WorkflowProcess):
             identifier = "de.csc.relhum_esgf",
             title="Specific to relative humidity (incl file selection)",
             version = "0.1",
-            #storeSupported = "true",   # async
-            #statusSupported = "true",  # retrieve status, needs to be true for async 
-            ## TODO: what can i do with this?
             metadata=[
                 {"title":"Foobar","href":"http://foo/bar"},
                 {"title":"Barfoo","href":"http://bar/foo"},
@@ -31,10 +30,10 @@ class RelHumProcessEsgf(WorkflowProcess):
         # ------------------
 
        
-        self.file_out = self.addComplexOutput(
-            identifier="file_out",
-            title="Out File",
-            abstract="out file",
+        self.output = self.addComplexOutput(
+            identifier="output",
+            title="Relative Humidity",
+            abstract="Calculated Relative Humidity",
             formats=[{"mimeType":"application/netcdf"}],
             asReference=True,
             )         
@@ -42,7 +41,12 @@ class RelHumProcessEsgf(WorkflowProcess):
     def execute(self):
         from Scientific.IO.NetCDF import NetCDFFile
         from os import curdir, path
-        
+
+        # default var names
+        huss = 'huss'
+        ps = 'ps'
+
+        # guess var names of files
         nc_files = self.get_nc_files()
         for nc_file in nc_files: 
             ds = NetCDFFile(nc_file)
@@ -52,19 +56,45 @@ class RelHumProcessEsgf(WorkflowProcess):
                 nc_huss = nc_file
             elif "hus" in ds.variables.keys():
                 nc_huss = nc_file
+                huss = 'hus'
             elif "ps" in ds.variables.keys():
                 nc_ps = nc_file
             elif "psl" in ds.variables.keys():
                 nc_ps = nc_file
+                ps = 'psl'
             else:
-                raise Exception("missing something")
+                raise Exception("input netcdf file has not variable tas|huss|ps")
                 
-        nc_hurs = path.join(path.abspath(curdir), "hurs_test.nc")
-        rel_hum_command = path.join(path.dirname(__file__),"rel_hum_esgf.sh")
-        self.cmd(cmd=["bash", rel_hum_command , nc_tas, nc_huss, nc_ps, nc_hurs ], stdout=True)
+        # merge ps and huss
+        (_, merged_ps_huss) = tempfile.mkstemp(suffix='.nc')
+        cmd = ['cdo', '-O', 'merge', nc_ps, nc_huss, merged_ps_huss]
+        self.cmd(cmd=cmd, stdout=True)
+
+        self.status.set(msg="relhum merged", percentDone=20, propagate=True)
         
-        self.status.set(msg="cdo sinfo done", percentDone=90, propagate=True)
-        self.file_out.setValue( nc_hurs )
+        # ps * huss
+        (_, nc_e) = tempfile.mkstemp(suffix='.nc')
+        expr = "expr,\'e=((%s*%s)/62.2)\'" % (ps, huss)
+        cmd = ['cdo', expr, merged_ps_huss, nc_e]
+        self.cmd(cmd=cmd, stdout=True)
+
+        self.status.set(msg="relhum ps*hus", percentDone=40, propagate=True)
+        
+        # partial vapour pressure using Magnus-Formula over water
+        # cdo expr,'es=6.1078*10^(7.5*(tas-273.16)/(237.3+(tas-273.16)))' ../in/tas_$filename  ../out/es_$filename
+        (_, nc_es) = tempfile.mkstemp(suffix='.nc')
+        cmd = ['cdo', "expr,\'es=6.1078*exp(17.08085*(tas-273.16)/(234.175+(tas-273.16)))\'", nc_tas, nc_es]
+        self.cmd(cmd=cmd, stdout=True)
+
+        self.status.set(msg="relhum ps*hus", percentDone=60, propagate=True)
+        
+        # calculate relative humidity
+        nc_hurs = path.join(path.abspath(curdir), "hurs_test.nc")
+        cmd = ['cdo', '-div', nc_e, nc_es, nc_hurs]
+        self.cmd(cmd=cmd, stdout=True)
+        
+        self.status.set(msg="relhum done", percentDone=90, propagate=True)
+        self.output.setValue( nc_hurs )
         
         
         
