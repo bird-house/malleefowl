0cfc283e9250f9708cb51ea9a33d483f4401d24a
Carsten Ehbrecht <carsten@localhost>
2013-09-05 23:47:53 +0200

    list and get local files added

--- processes/source.py
+++ processes/source.py
@@ -5,45 +5,89 @@ Author: Carsten Ehbrecht (ehbrecht@dkrz.de)
 """
 
 import os
+import json
 
-from malleefowl.process import SourceProcess
+from malleefowl.process import SourceProcess, WPSProcess
 
 
-class Filesystem(SourceProcess):
-    """This process provides files from local filesystem."""
+class ListLocalFiles(WPSProcess):
+    """This process lists files from local filesystem."""
 
     def __init__(self):
-        SourceProcess.__init__(self,
-            identifier = "org.malleefowl.files",
-            title = "Choose local files",
+        WPSProcess.__init__(self,
+            identifier = "org.malleefowl.listfiles",
+            title = "List local files",
             version = "0.1",
             metadata=[
                 {"title":"ESGF","href":"http://esgf.org"},
                 ],
-            abstract="Choose local files")
+            abstract="List local files")
 
         # openid
         # -----------
 
-        files = [f for f in os.listdir(self.files_path)]
-
-        self.file_in = self.addLiteralInput(
-            identifier = "file",
-            title = "File",
-            abstract = "Choose File",
+        self.filter_in = self.addLiteralInput(
+            identifier = "filter",
+            title = "Filter",
+            abstract = "Filter for file selection",
             minOccurs = 1,
-            maxOccurs = 10,
+            maxOccurs = 1,
             type = type(''),
-            allowedValues=files
+            )
+
+        self.filelist_out = self.addLiteralOutput(
+            identifier="output",
+            title="Filelist as json",
+            abstract="This is a filelist as json",
+            type=type('')
             )
 
     def execute(self):
         self.status.set(msg="starting ...", percentDone=5, propagate=True)
 
-        file = self.file_in.getValue()
+        filter = self.filter_in.getValue()
 
         self.status.set(msg="retrieved file", percentDone=90, propagate=True)
+
+        files = [f for f in os.listdir(self.files_path) if filter in f]
         
-        self.output.setValue(path)
+        self.filelist_out.setValue(json.dumps(files))
+
+class GetLocalFile(SourceProcess):
+    """This process retrieves files from local filesystem."""
+
+    def __init__(self):
+        SourceProcess.__init__(self,
+            identifier = "org.malleefowl.getfile",
+            title = "Get local file",
+            version = "0.1",
+            metadata=[
+                {"title":"ESGF","href":"http://esgf.org"},
+                ],
+            abstract="Get local file")
+
+        # openid
+        # -----------
+
+        self.filter_in = self.addLiteralInput(
+            identifier = "filter",
+            title = "Filter",
+            abstract = "Filter for file selection",
+            minOccurs = 1,
+            maxOccurs = 1,
+            type = type(''),
+            )
+
+    def execute(self):
+        self.status.set(msg="starting ...", percentDone=5, propagate=True)
+
+        filter = self.filter_in.getValue()
+
+        self.status.set(msg="retrieved file", percentDone=90, propagate=True)
+
+        files = [f for f in os.listdir(self.files_path) if filter in f]
+        file_path = os.path.join(self.files_path, files[0])
+        
+        self.output.setValue(file_path)
 
 
