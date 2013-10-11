aa3139bd121e98fb234e61ba450a065c0500307e
Tobias Kipp <kipp@dkrz.de>
2013-10-11 11:40:30 +0200

    Changed the process to generate a file with some text and added a boolean output with a dummy function that will show if the Quality Check is fine with the file(s).

--- processes/qualitycontrol.py
+++ processes/qualitycontrol.py
@@ -1,47 +1,74 @@
 
 """
-Test Process kept simple to learn the WPS structure
-
+Quality Control process. It calls the Quality Control scripts and returns the 
+log file for now.
 Author: Tobias Kipp (kipp@dkrz.de)
 """
 import types
+import datetime
 from pywps.Process import WPSProcess                                
+#from malleefowl.process import WPSProcess
 class Process(WPSProcess):
-     def __init__(self):
-          # init process
-         abstractMultiLine = ("The Test process is a simple process to learn how to use"+
-                               "the WPS structure. The process will accept 2 input numbers"+
-                               "and will calculate the product of both.")
-         
+    def __init__(self):
+         # init process
+        abstractML =("The process takes in configuration files and a link"
+                     +" to a to check file. The output is the log file for now.")
 
-         WPSProcess.__init__(self,
-              identifier = "QualityControl", 
-              title="TestTitle",
-              version = "0.1",
-              storeSupported = "true",
-              statusSupported = "true",
-              abstract=abstractMultiLine,                    
-              grassLocation =False)
-              
-         self.Input1 = self.addLiteralInput(identifier = "int",
-                                            title = "Input1 number", 
-                                            default=100,
-                                            type=type(1),
-                                            minOccurs = 1,#required
-                                            maxOccurs = 1,
-                                            )
-         self.Input2 = self.addLiteralInput(identifier="string", 
-                                            title="Some text", 
-                                            #tefault="A B C 350",
-                                            type=types.StringType,
-                                            minOccurs = 1,#required
-                                            maxOccurs = 1,
-                                            )
-         self.Output= self.addLiteralOutput(identifier="output", 
-                                            title="Output is an integer-string product.",
-                                            default = None,
-                                            )
-     def execute(self):
-         
-         self.Output.setValue(self.Input1.getValue()*str(self.Input2.getValue()))
-         return #If execute() returns anything but null, it is considered as error and exception is called
+        WPSProcess.__init__(self,
+            identifier = "QualityControl", 
+            title="Quality Control",
+            version = "0.1",
+            storeSupported = "true",
+            statusSupported = "true",
+            abstract=abstractML,
+            grassLocation=False)
+             
+        self.configs = self.addLiteralInput(identifier = "config",
+                                           title = "Configuration files", 
+                                           default="configuration file here",
+                                           type=type(""),
+                                           minOccurs = 1,#required
+                                           #maxOccurs = 1,
+                                           )
+        self.files = self.addLiteralInput(identifier="file links", 
+                                           title="Links to files", 
+                                           default="Links here",
+                                           type=types.StringType,
+                                           minOccurs = 1,#required
+                                           maxOccurs = 1,
+                                           )
+        self.output = self.addComplexOutput(
+            identifier="output",
+            title="Output",
+            abstract="Quality check result log.",
+            metadata=[],
+            formats=[{"mimeType":"text/plain"}],
+# {"miMeTy pe": "text/xml"}, {"mimeType": "application/json"}],
+            asReference=True,
+            )
+        self.allOk = self.addLiteralOutput(identifier="allOk",
+                                           title ="Everything is ok",
+                                           abstract ="True if the Quality Check did not find errors.",
+                                           default=False,
+                                           type = type(True),
+                                          )
+    def execute(self):
+        
+        self.status.set(msg="Creating new file", percentDone=50, propagate=True)
+        filename=str(datetime.datetime.now()).replace(":","-").replace(" ","_").replace(".","_")
+        output_ext = "log"
+        filename+=output_ext
+        #file1 = self.mktempfile(suffix='.' + output_ext)
+        file1 = file(filename,"w")
+        self.status.set(msg="Writing to file", percentDone=80, propagate=True)
+        file1.write("The file name is:"+filename)
+        self.output.setValue(file1)
+        self.allOk.setValue(self.evaluateResults())
+        return #If execute() returns anything but null, it is considered as error and exception is called
+    """ Return true if the Quality Check finds no errors.
+        TODO: Implement it
+    """
+    def evaluateResults(self):
+        return True
+	    
+	         
