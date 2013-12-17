f165c72fa07d6259987a1299d1ea4a669339817c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2013-12-17 19:18:47 +0100

    publish and retrieve local files with openid. not working yet

--- processes/source.py
+++ processes/source.py
@@ -7,8 +7,8 @@ Author: Carsten Ehbrecht (ehbrecht@dkrz.de)
 import os
 import json
 
-#from malleefowl.process import SourceProcess, WPSProcess
-import malleefowl.process
+import malleefowl
+from malleefowl import utils
 
 
 class ListLocalFiles(malleefowl.process.WPSProcess):
@@ -23,6 +23,15 @@ class ListLocalFiles(malleefowl.process.WPSProcess):
                 {"title":"ESGF","href":"http://esgf.org"},
                 ],
             abstract="List local files")
+
+        self.openid_in = self.addLiteralInput(
+            identifier = "openid",
+            title = "ESGF OpenID",
+            abstract = "Enter ESGF OpenID",
+            minOccurs = 1,
+            maxOccurs = 1,
+            type = type('')
+            )
 
         self.filter_in = self.addLiteralInput(
             identifier = "filter",
@@ -43,11 +52,15 @@ class ListLocalFiles(malleefowl.process.WPSProcess):
     def execute(self):
         self.status.set(msg="starting ...", percentDone=5, propagate=True)
 
+        user_id = utils.user_id(self.openid_in.getValue())
+        files_path = os.path.join(self.files_path, user_id)
+        utils.mkdir(files_path)
+
         filter = self.filter_in.getValue()
 
-        self.status.set(msg="retrieved file", percentDone=90, propagate=True)
+        files = [f for f in os.listdir(files_path) if filter in f]
 
-        files = [f for f in os.listdir(self.files_path) if filter in f]
+        self.status.set(msg="retrieved file list", percentDone=90, propagate=True)
         
         self.filelist_out.setValue(json.dumps(files))
 
@@ -64,13 +77,26 @@ class GetFileFromFilesystem(malleefowl.process.SourceProcess):
                 ],
             abstract="Get file from filesystem storage")
 
+        self.openid_in = self.addLiteralInput(
+            identifier = "openid",
+            title = "ESGF OpenID",
+            abstract = "Enter ESGF OpenID",
+            minOccurs = 1,
+            maxOccurs = 1,
+            type = type('')
+            )
+
     def execute(self):
         self.status.set(msg="starting ...", percentDone=5, propagate=True)
 
+        user_id = utils.user_id(self.openid_in.getValue())
+        files_path = os.path.join(self.files_path, user_id)
+        utils.mkdir(files_path)
+
         file_id = self.file_identifier.getValue()
 
-        files = [f for f in os.listdir(self.files_path) if file_id in f]
-        file_path = os.path.join(self.files_path, files[0])
+        files = [f for f in os.listdir(files_path) if file_id in f]
+        file_path = os.path.join(files_path, files[0])
 
         self.status.set(msg="retrieved file", percentDone=90, propagate=True)
         
