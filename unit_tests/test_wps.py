74c9e39a02fd5dd87c50f8c8d702053251cce7c6
Carsten Ehbrecht <ehbrecht@dkrz.de>
2013-12-17 13:49:36 +0100

    testing dummy process

--- unit_tests/test_wps.py
+++ unit_tests/test_wps.py
@@ -4,12 +4,12 @@ from nose import SkipTest
 import pywps
 from xml.dom import minidom
 
-getcapabilitiesrequest = "service=wps&request=getcapabilities"
+caps_request = "service=wps&request=getcapabilities"
 wpsns = "http://www.opengis.net/wps/1.0.0"
 
 def test_caps():
     mypywps = pywps.Pywps(pywps.METHOD_GET)
-    inputs = mypywps.parseRequest(getcapabilitiesrequest)
+    inputs = mypywps.parseRequest(caps_request)
     mypywps.performRequest(inputs)
     xmldom = minidom.parseString(mypywps.response)
     nose.tools.ok_(len(xmldom.getElementsByTagNameNS(wpsns,"Process")) > 0)
