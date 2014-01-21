f40dd5e60ef61980bfb36af7b44fcd3eb2f5c360
Tobias Kipp <kipp@dkrz.de>
2014-01-21 14:13:38 +0100

    Added outputs to the Quality Control process. To get the return value of the process a Queue object is passed.

--- processes/quality_processes.py
+++ processes/quality_processes.py
@@ -8,7 +8,7 @@ import types
 import malleefowl.process 
 import qc_processes.qcprocesses as qcprocesses
 
-from multiprocessing import Process, Pipe
+from multiprocessing import Process, Pipe, Queue
 
 class QualityControlProcess(malleefowl.process.WPSProcess):
     """
@@ -21,7 +21,8 @@ class QualityControlProcess(malleefowl.process.WPSProcess):
         status_conn, qc_conn = self.pipe
         self.printmethod = qc_conn.send
         self.parallel_id = "web1"#TODO set as parameter
-        self.database_location = "/home/tk/sandbox/databases/pidinfo.db"#TODO fixed relative to climdaps.
+        self.database_location = "/home/tk/sandbox/databases/pidinfo.db"#TODO relative to climdaps.
+       
 
         malleefowl.process.WPSProcess.__init__(self,
             identifier = "QC_Quality_Control",
@@ -42,7 +43,7 @@ class QualityControlProcess(malleefowl.process.WPSProcess):
             identifier = "args",
             title="Additional QC parameters",
             abstract = "Using options of the QC tool. (e.g. -E_SELECT .*)",
-            default = str("--pb "),
+            #default = str("--pb "),
             minOccurs = 0,
             maxOccurs = 1,
             type=types.StringType,
@@ -99,10 +100,64 @@ class QualityControlProcess(malleefowl.process.WPSProcess):
             type=types.StringType,
             )
         
+        self.qc_call_exit_code = self.addLiteralOutput(
+            identifier="qc_call_exit_code",
+            title ="qcManager exit code",
+            abstract ="Exit code of the quality control tool.",
+            type = types.StringType,
+            )
+
+        self.qc_call = self.addLiteralOutput(
+            identifier="qc_call",
+            title="qc_call",
+            type=types.StringType,
+            )
+        self.fail_count = self.addLiteralOutput(
+            identifier="fail_count",
+            title="Fail count",
+            type=types.IntType,
+            )
+        self.omit_count = self.addLiteralOutput(
+            identifier="omit_count",
+            title="Omit count",
+            type=types.IntType,
+            )
+        self.pass_count = self.addLiteralOutput(
+            identifier="pass_count",
+            title="Pass count",
+            type=types.IntType,
+            )
+        self.fixed_count = self.addLiteralOutput(
+            identifier="fixed_count",
+            title="Fixed count",
+            type=types.IntType,
+            )
+
+        self.has_issues = self.addLiteralOutput(
+            identifier="has_issues",
+            title="There is something wrong with the checked files.",
+            type=types.BooleanType,
+            )
+
+        self.process_log = self.addComplexOutput(
+            identifier="process_log",
+            title="Log of this process.",
+            metadata=[],
+            formats=[{"mimeType":"text/plain"}],
+            asReference=True,
+            )
+        self.to_publish_qc_files = self.addComplexOutput(
+            identifier="to_publish_qc_files",
+            title="QC files that need to be published",
+            metadata=[],
+            formats=[{"mimeType":"text/plain"}],
+            asReference=True,
+            )
 
 
     def execute(self):
         self.status.set(msg="Initiate process", percentDone=0, propagate=True)
+        output=dict()
         param_dict = dict(project_data_dir = self.project_data_dir.getValue(),
                           args = self.args.getValue(),
                           project= self.project.getValue(),
@@ -111,7 +166,8 @@ class QualityControlProcess(malleefowl.process.WPSProcess):
                           access=self.access.getValue(),
                           metadata_format = self.metadata_format.getValue(),
                           replica = self.replica.getValue(),
-                          latest = self.latest.getValue())
+                          latest = self.latest.getValue(),
+                          queue = Queue())
 
         qcp = qcprocesses.QCProcesses(self.database_location,
                                       self.parallel_id,
@@ -119,6 +175,23 @@ class QualityControlProcess(malleefowl.process.WPSProcess):
                                       work_dir = "/home/tk/sandbox/climdaps/var/qc_cache/"
                                       )
         _run_process(qcp.quality_control,kwargs=param_dict,wpsprocess=self)
+
+        output = param_dict["queue"].get()
+        self.qc_call_exit_code.setValue(output["qc_info"])
+        self.qc_call.setValue(output["qc_call"])
+        self.fail_count.setValue(output["fail_count"])
+        self.pass_count.setValue(output["pass_count"])
+        self.omit_count.setValue(output["omit_count"])
+        self.fixed_count.setValue(output["fixed_count"])
+        self.has_issues.setValue(output["has_issues"])
+
+        process_log = _create_server_copy_of_file(output["process_log"],self)
+        self.process_log.setValue(process_log)
+        to_publish_qc_files_log = _create_server_copy_of_file(output["to_publish_qc_files_log"],self)
+        self.to_publish_qc_files.setValue(to_publish_qc_files_log)
+
+
+
         return
 
 ##################
@@ -145,3 +218,13 @@ def _run_process(target,kwargs,wpsprocess):
                except:
                    pass#it is not of "status: current max" format. Ignore it.
     
+
+def _create_server_copy_of_file(filename,wpsprocess):
+    serverfile =  open(wpsprocess.mktempfile(suffix=".txt"),"w")
+    localfile = open(filename,"r")
+    serverfile.write(localfile.read())
+    localfile.close()
+    serverfile.close()
+    return serverfile
+
+
