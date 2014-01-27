5b7950072f390971e31d0ca283ec8c5f8659babd
Tobias Kipp <kipp@dkrz.de>
2014-01-27 14:29:38 +0100

    Changed the user directory structure. Added two processes that combined are equal to QualityControlProcess.

--- processes/quality_processes.py
+++ processes/quality_processes.py
@@ -118,6 +118,272 @@ class PidGenerationProcess(malleefowl.process.WPSProcess):
         return 
 
 
+class QualityCheckProcess(malleefowl.process.WPSProcess):
+    def __init__(self):
+
+        self.pipe = Pipe()
+        status_conn, qc_conn = self.pipe
+        self.printmethod = qc_conn.send
+        self.database_location = DATABASE_LOCATION
+
+        malleefowl.process.WPSProcess.__init__(self,
+            identifier = "QC_Quality_Check",
+            title="Quality Check using qc_processes",
+            version="2014.01.27",
+            metadata=[],
+            abstract="Runs a quality check on a given folder.")
+
+        self.parallel_id = self.addLiteralInput(
+            identifier="parallel_id",
+            title="Parallel ID",
+            abstract=("An ID for the current process. If multiple processes are running in parallel "
+                      +"choose an unused one."),
+            default ="web1",
+            type=types.StringType,
+            )
+        self.username = self.addLiteralInput(
+            identifier = "username",
+            title = "Username",
+            abstract = ("The username is used to prevent two users working in the same directory."
+                        +" Currently this is a work around until the username can be derived "
+                        +"automatically."),
+            default = "defaultuser",
+            type = types.StringType,
+            )
+        self.project_data_dir = self.addLiteralInput(
+            identifier="project_data_dir",
+            title="To analyse data path",
+            abstract="A local path",
+            default=os.path.join(climdapsabs,"examples/data/CORDEX"),
+            type=types.StringType,
+            )
+
+        self.args = self.addLiteralInput(
+            identifier = "args",
+            title="Additional QC parameters",
+            abstract = "Using options of the QC tool. (e.g. -E_SELECT .*)",
+            minOccurs = 0,
+            maxOccurs = 1,
+            type=types.StringType,
+            )
+
+        self.project = self.addLiteralInput(
+            identifier="project",
+            title="The project used.",
+            abstract="Currently only CORDEX is fully supported.",
+            default="CORDEX",
+            type=types.StringType,
+            )
+
+
+        self.clean_process_dir = self.addLiteralInput(
+            identifier ="clean_process_dir",
+            title="clean work",
+            abstract=("Remove data from the working directory. Quality Check skips already checked"+ 
+              " files. After clean up it will check all files."),
+            type=types.BooleanType,
+            )
+        
+        self.qc_call_exit_code = self.addLiteralOutput(
+            identifier="qc_call_exit_code",
+            title ="qcManager exit code",
+            abstract ="Exit code of the quality control tool.",
+            type = types.StringType,
+            )
+
+        self.qc_call = self.addLiteralOutput(
+            identifier="qc_call",
+            title="qc_call",
+            type=types.StringType,
+            )
+
+
+    def execute(self):
+        self.status.set(msg="Initiate process", percentDone=0, propagate=True)
+        param_dict = dict(project_data_dir = self.project_data_dir.getValue(),
+                          args = self.args.getValue(),
+                          project= self.project.getValue(),
+                          queue = Queue())
+
+        qcp = qcprocesses.QCProcesses(self.database_location,
+                                      username = self.username.getValue(),
+                                      parallel_id = self.parallel_id.getValue(),
+                                      printmethod=self.printmethod,
+                                      work_dir = WORK_DIR
+                                      )
+        if self.clean_process_dir.getValue() == True:
+            qcp.clean_process_dir()
+
+        _run_process(qcp.quality_check,kwargs=param_dict,wpsprocess=self)
+
+        output = param_dict["queue"].get()
+        self.qc_call_exit_code.setValue(output["qc_call_exit_code"])
+        self.qc_call.setValue(output["qc_call"])
+
+        return
+
+class EvaluateQualityCheckProcess(malleefowl.process.WPSProcess):
+    """
+    The process runs the qc_processes QualityControl method and 
+    handles the progress bar depending on the output of the method.
+    """
+    def __init__(self):
+
+        self.pipe = Pipe()
+        status_conn, qc_conn = self.pipe
+        self.printmethod = qc_conn.send
+        self.database_location = DATABASE_LOCATION
+
+        malleefowl.process.WPSProcess.__init__(self,
+            identifier = "QC_Evaluate_Quality_Check",
+            title="Evaluate Quality Check using qc_processes",
+            version="2014.01.27",
+            metadata=[],
+            abstract="Evaluates the quality check and generates metadata and quality files")
+
+        self.parallel_id = self.addLiteralInput(
+            identifier="parallel_id",
+            title="Parallel ID",
+            abstract=("An ID for the current process. If multiple processes are running in parallel "
+                      +"choose an unused one."),
+            default ="web1",
+            type=types.StringType,
+            )
+        self.username = self.addLiteralInput(
+            identifier = "username",
+            title = "Username",
+            abstract = ("The username is used to prevent two users working in the same directory."
+                        +" Currently this is a work around until the username can be derived "
+                        +"automatically."),
+            default = "defaultuser",
+            type = types.StringType,
+            )
+
+        self.data_node= self.addLiteralInput(
+            identifier="data_node",
+            title="Data node",
+            default="ipcc-ar5.dkrz.de",
+            type=types.StringType,
+            )
+
+        self.index_node= self.addLiteralInput(
+            identifier="index_node",
+            title="Index node",
+            default="esgf-data.dkrz.de",
+            type=types.StringType,
+            )
+
+        self.access=self.addLiteralInput(
+            identifier="access",
+            title="Access",
+            default="HTTPServer",
+            type=types.StringType,
+            )
+
+        self.metadata_format=self.addLiteralInput(
+            identifier="metadata_format",
+            title="metadata_format",
+            default="THREDDS",
+            type=types.StringType,
+            )
+
+        self.replica= self.addLiteralInput(
+            identifier="replica",
+            title="Replica",
+            default="false",
+            type=types.StringType,
+            )
+            
+        self.latest= self.addLiteralInput(
+            identifier="latest",
+            title="Latest",
+            default="true",
+            type=types.StringType,
+            )
+
+        
+        self.found_tags = self.addLiteralOutput(
+            identifier="found_tags",
+            title="found_tags",
+            type=types.StringType,
+            )
+        self.fail_count = self.addLiteralOutput(
+            identifier="fail_count",
+            title="Fail count",
+            type=types.IntType,
+            )
+        self.omit_count = self.addLiteralOutput(
+            identifier="omit_count",
+            title="Omit count",
+            type=types.IntType,
+            )
+        self.pass_count = self.addLiteralOutput(
+            identifier="pass_count",
+            title="Pass count",
+            type=types.IntType,
+            )
+        self.fixed_count = self.addLiteralOutput(
+            identifier="fixed_count",
+            title="Fixed count",
+            type=types.IntType,
+            )
+
+        self.has_issues = self.addLiteralOutput(
+            identifier="has_issues",
+            title="There is something wrong with the checked files.",
+            type=types.BooleanType,
+            )
+
+        self.process_log = self.addComplexOutput(
+            identifier="process_log",
+            title="Log of this process.",
+            metadata=[],
+            formats=[{"mimeType":"text/plain"}],
+            asReference=True,
+            )
+        self.to_publish_qc_files = self.addComplexOutput(
+            identifier="to_publish_qc_files",
+            title="QC files that need to be published",
+            metadata=[],
+            formats=[{"mimeType":"text/plain"}],
+            asReference=True,
+            )
+
+
+    def execute(self):
+        self.status.set(msg="Initiate process", percentDone=0, propagate=True)
+        param_dict = dict(
+                          data_node=self.data_node.getValue(),
+                          index_node=self.index_node.getValue(),
+                          access=self.access.getValue(),
+                          metadata_format = self.metadata_format.getValue(),
+                          replica = self.replica.getValue(),
+                          latest = self.latest.getValue(),
+                          queue = Queue())
+
+        qcp = qcprocesses.QCProcesses(self.database_location,
+                                      username = self.username.getValue(),
+                                      parallel_id = self.parallel_id.getValue(),
+                                      printmethod=self.printmethod,
+                                      work_dir = WORK_DIR
+                                      )
+
+        _run_process(qcp.evaluate_quality_check,kwargs=param_dict,wpsprocess=self)
+
+        output = param_dict["queue"].get()
+        self.fail_count.setValue(output["fail_count"])
+        self.pass_count.setValue(output["pass_count"])
+        self.omit_count.setValue(output["omit_count"])
+        self.fixed_count.setValue(output["fixed_count"])
+        self.has_issues.setValue(output["has_issues"])
+        self.found_tags.setValue(output["found_tags"])
+
+        process_log = _create_server_copy_of_file(output["process_log"],self)
+        self.process_log.setValue(process_log)
+        to_publish_qc_files_log = _create_server_copy_of_file(output["to_publish_qc_files_log"],self)
+        self.to_publish_qc_files.setValue(to_publish_qc_files_log)
+        return
+
 class QualityControlProcess(malleefowl.process.WPSProcess):
     """
     The process runs the qc_processes QualityControl method and 
@@ -128,7 +394,6 @@ class QualityControlProcess(malleefowl.process.WPSProcess):
         self.pipe = Pipe()
         status_conn, qc_conn = self.pipe
         self.printmethod = qc_conn.send
-        self.parallel_id = "web1"#TODO set as parameter
         self.database_location = DATABASE_LOCATION
 
         malleefowl.process.WPSProcess.__init__(self,
@@ -138,6 +403,23 @@ class QualityControlProcess(malleefowl.process.WPSProcess):
             metadata=[],
             abstract="Runs a quality check on a given folder and generates metadata and quality files")
 
+        self.parallel_id = self.addLiteralInput(
+            identifier="parallel_id",
+            title="Parallel ID",
+            abstract=("An ID for the current process. If multiple processes are running in parallel "
+                      +"choose an unused one."),
+            default ="web1",
+            type=types.StringType,
+            )
+        self.username = self.addLiteralInput(
+            identifier = "username",
+            title = "Username",
+            abstract = ("The username is used to prevent two users working in the same directory."
+                        +" Currently this is a work around until the username can be derived "
+                        +"automatically."),
+            default = "defaultuser",
+            type = types.StringType,
+            )
         self.project_data_dir = self.addLiteralInput(
             identifier="project_data_dir",
             title="To analyse data path",
@@ -288,7 +570,8 @@ class QualityControlProcess(malleefowl.process.WPSProcess):
                           queue = Queue())
 
         qcp = qcprocesses.QCProcesses(self.database_location,
-                                      parallel_id = self.parallel_id,
+                                      username = self.username.getValue(),
+                                      parallel_id = self.parallel_id.getValue(),
                                       printmethod=self.printmethod,
                                       work_dir = WORK_DIR
                                       )
@@ -337,13 +620,27 @@ class QualityPublisherProcess(malleefowl.process.WPSProcess):
             default="esgf-dev",
             type=types.StringType,
             )
+        
            
-        self.filename_from_qualitycontrol = self.addLiteralInput(
-            identifier = "filename_from_qualitycontrol",
-            title = "The file containing the generated quality results file names.",
-            default = "",
+        self.parallel_id = self.addLiteralInput(
+            identifier="parallel_id",
+            title="Parallel ID",
+            abstract=("An ID for the current process. The processes folder contains the to upload files"
+                      +" list."),
+            default ="web1",
             type=types.StringType,
             )
+
+        self.username = self.addLiteralInput(
+            identifier = "username",
+            title = "Username",
+            abstract = ("The username is used to prevent two users working in the same directory."
+                        +" Currently this is a work around until the username can be derived "
+                        +"automatically."),
+            default = "defaultuser",
+            type = types.StringType,
+            )
+            
 
         self.process_log = self.addComplexOutput(
             identifier = "process_log",
@@ -354,12 +651,13 @@ class QualityPublisherProcess(malleefowl.process.WPSProcess):
     def execute(self):
         self.status.set(msg="Initiate process", percentDone=0, propagate=True)
         param_dict = dict(ssh_name=self.ssh_name.getValue(),
-                          to_publish_qc_log = self.filename_from_qualitycontrol.getValue(),
                           queue = Queue())
 
         qcp = qcprocesses.QCProcesses(self.database_location,
+                                      username = self.username.getValue(),
                                       printmethod=self.printmethod,
-                                      work_dir = WORK_DIR
+                                      work_dir = WORK_DIR,
+                                      parallel_id = self.parallel_id.getValue(),
                                       )
         _run_process(qcp.qualitypublisher,kwargs=param_dict,wpsprocess=self)
 
