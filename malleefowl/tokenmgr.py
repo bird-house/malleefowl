60e311a8e39d447f84ced38b4842b6abe72249eb
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-03-07 13:06:13 +0100

    working malleefowl tokenmgr

---
+++ malleefowl/tokenmgr.py
@@ -0,0 +1,73 @@
+##
+## This module manages tokens to access wps processes.
+##
+##
+## TODO: use a standard python module ... maybe for oauth2?
+
+from malleefowl import wpslogging as logging
+logger = logging.getLogger(__name__)
+
+
+class AccessDeniedError(Exception):
+    pass
+
+class UnknownUserIDError(Exception):
+    pass
+
+class UnknownTokenError(Exception):
+    pass
+
+TOKEN_MAP = {}
+USERID_MAP = {}
+
+class checkAccess(object):
+
+    def __init__(self, f):
+        self.f = f
+
+    def __call__(self, access_token, other):
+        if not _is_sys_token(access_token):
+            logger.warn('function "%s" called with invalid access token "%s"' % (self.f.__name__, access_token))
+            raise AccessDeniedError
+        return self.f(access_token, other)
+
+@property
+def sys_user():
+    return 'malleefowl'
+
+def init(token):
+    _add(token, sys_user)
+
+@checkAccess
+def get_token(access_token, userid):
+    token = USERID_MAP.get(userid, None)
+    if token is None:
+        raise UnknownUserIDError
+    return token
+
+@checkAccess
+def gen_token(access_token, userid):
+    token = userid.replace('@', '_')
+    _add(token, userid)
+    return token
+
+@checkAccess
+def get_userid(access_token, token):
+    userid= TOKEN_MAP.get(token, None)
+    if userid is None:
+        raise UnknownTokenError
+    return userid
+
+@checkAccess
+def is_token_valid(access_token, token):
+    return TOKEN_MAP.has_key(token)
+
+def _is_sys_token(token):
+    return TOKEN_MAP.get(token, None) == sys_user
+
+def _add(token, userid):
+    global TOKEN_MAP, USERID_MAP
+    TOKEN_MAP[token] = userid
+    USERID_MAP[userid] = token
+
+
---
+++ unit_tests/test_tokenmgr.py
@@ -0,0 +1,38 @@
+from nose.tools import ok_, with_setup, raises
+from nose import SkipTest
+from nose.plugins.attrib import attr
+
+from malleefowl import tokenmgr
+from malleefowl.tokenmgr import AccessDeniedError
+
+def setup():
+    tokenmgr.init('abc')
+    tokenmgr._add('abc123', 'amelie@montematre.org')
+    
+def test_token_valid():
+    ok_(tokenmgr.is_token_valid('abc', 'abc'))
+    ok_(not tokenmgr.is_token_valid('abc', 'dfe'))
+
+@raises(AccessDeniedError)
+def test_token_valid():
+    ok_(tokenmgr.is_token_valid('abc123', 'abc'))
+
+@raises(AccessDeniedError)
+def test_token_valid_with_exception():
+    ok_(tokenmgr.is_token_valid('abc123', 'abc'))
+
+def test_is_sys_token():
+    ok_(tokenmgr._is_sys_token('abc'))
+    ok_(not tokenmgr._is_sys_token('dfe'))
+
+def test_gen_token():
+    token = tokenmgr.gen_token('abc', 'adelie@somewhere.org')
+
+@raises(AccessDeniedError)
+def test_gen_token_with_exception():
+    token = tokenmgr.gen_token('abc123', 'adelie@somewhere.org')
+
+def test_get_token():
+    token = tokenmgr.get_token('abc', 'amelie@montematre.org')
+    ok_(token == 'abc123')
+    
