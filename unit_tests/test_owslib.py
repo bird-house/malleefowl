4a15064f4ce5df96376aeaef08317992367e1ecb
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-04-14 18:25:23 +0200

    updte test owslib

--- unit_tests/test_owslib.py
+++ unit_tests/test_owslib.py
@@ -19,11 +19,11 @@ def test_inout():
     wps = WebProcessingService(base.SERVICE, verbose=False)
     execution = wps.execute(
         'org.malleefowl.test.inout',
-        inputs=[('string', 'me@localhost.de'), ('intRequired', '0')],
+        inputs=[('string', TEST_USERID), ('intRequired', '0')],
         output=[('string', False)])
     monitorExecution(execution, sleepSecs=1)
     
     result = execution.processOutputs[0].data[0]
-    nose.tools.ok_('me@localhost.de' in result, result)
+    nose.tools.ok_(TEST_USERID in result, result)
     
    
