4dda57438ec6f209d0a18c15ee4f99c060ffa4d5
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-05-19 17:51:39 +0200

    visualisation test added to integration tests

--- unit_tests/test_integration.py
+++ unit_tests/test_integration.py
@@ -19,4 +19,18 @@ def test_inout():
     nose.tools.ok_(len(result) == 12, len(result))
     nose.tools.ok_('intRequired' in str(result), result)
 
+@attr('integration')
+def test_visualisation():
+    result = wpsclient.execute(
+        service = base.SERVICE,
+        identifier = "de.csc.visualisation.worker",
+        inputs = [('variable', 'tas'),
+                  ('file_identifier',
+                   'https://mouflon.dkrz.de/wpsoutputs/tutorial/tas_EUR-11_MPI-M-MPI-ESM-LR_historical_r1i1p1_SMHI-RCA4_v1_day_19700101-19701231.nc')],
+        outputs=[("output", True)]
+        
+        )
+    nose.tools.ok_(len(result) == 1, len(result))
+    nose.tools.ok_('html' in result[0]['reference'], result)
+
 
