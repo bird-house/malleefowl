c7aa62367cdab078d98a5b614e61778e4a30f9c4
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-05-19 17:56:31 +0200

    cdo sinfo test added to integration

--- unit_tests/test_integration.py
+++ unit_tests/test_integration.py
@@ -33,4 +33,17 @@ def test_visualisation():
     nose.tools.ok_(len(result) == 1, len(result))
     nose.tools.ok_('html' in result[0]['reference'], result)
 
+@attr('integration')
+def test_cdo_sinfo():
+    result = wpsclient.execute(
+        service = base.SERVICE,
+        identifier = "de.dkrz.cdo.sinfo.worker",
+        inputs=[('file_identifier',
+                 'https://mouflon.dkrz.de/wpsoutputs/tutorial/tas_EUR-11_MPI-M-MPI-ESM-LR_historical_r1i1p1_SMHI-RCA4_v1_day_19700101-19701231.nc')],
+        outputs=[("output", True)]
+        
+        )
+    nose.tools.ok_(len(result) == 1, len(result))
+    nose.tools.ok_('txt' in result[0]['reference'], result)
+
 
