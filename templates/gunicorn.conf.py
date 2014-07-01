c352a02e5e36dde6fe471ea5ae0e8febc9afa2fd
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-07-01 16:58:46 +0200

    cleaned up pywps config

--- templates/gunicorn.conf.py
+++ templates/gunicorn.conf.py
@@ -2,7 +2,7 @@ bind = '${bind}'
 workers = 3
 
 # environment
-raw_env = ["HOME=${pywps:home}", 
+raw_env = ["HOME=${buildout:anaconda-home}/var/lib/pywps", 
            "PYWPS_CFG=${malleefowl_config:output}", 
            "PATH=${buildout:anaconda-home}/bin:${buildout:bin-directory}:/usr/bin:/bin", 
            "GDAL_DATA=${env:gdal_data}"]                                                                                                               
