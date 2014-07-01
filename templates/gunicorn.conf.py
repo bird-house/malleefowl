a629c35f6cfe2498a086f194a011d709cd643e9f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-07-01 15:55:21 +0200

    using conda pywps, merged pywps.cfg into malleefowl.cfg, malleefowl wsgiwps.py added

--- templates/gunicorn.conf.py
+++ templates/gunicorn.conf.py
@@ -3,7 +3,7 @@ workers = 3
 
 # environment
 raw_env = ["HOME=${pywps:home}", 
-           "PYWPS_CFG=${pywps_config:output}", 
+           "PYWPS_CFG=${malleefowl_config:output}", 
            "PATH=${buildout:anaconda-home}/bin:${buildout:bin-directory}:/usr/bin:/bin", 
            "GDAL_DATA=${env:gdal_data}"]                                                                                                               
 
