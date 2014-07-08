23919e3d754d23caf6477f3ef98de5c3c43bb8a1
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-07-08 13:37:29 +0200

    upgrade: remove gunicorn

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ function upgrade() {
         sudo apt-get -y --force-yes purge nginx-full
         sudo apt-get -y --force-yes purge mongodb mongodb-server mongodb-clients mongodb-dev
         sudo apt-get -y --force-yes purge tomcat7
+        sudo apt-get -y --force-yes purge gunicorn
 
         sudo apt-get -y --force-yes autoremove
     elif [ -f /etc/redhat-release ] ; then
