6184ce139c2dea12896c630e20d786f23aac6259
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-08-21 15:01:23 +0200

    added restflow generate_and_run added

--- malleefowl/processes/restflow.py
+++ malleefowl/processes/restflow.py
@@ -1,12 +1,3 @@
-"""
-Processes for testing wps data types
-
-Author: Carsten Ehbrecht (ehbrecht@dkrz.de)
-"""
-
-import os.path
-import types
-
 from malleefowl import wpslogging as logging
 logger = logging.getLogger(__name__)
 
@@ -138,6 +129,75 @@ class Run(WPSProcess):
             json.dump(obj=result, fp=fp, indent=4, sort_keys=True)
         self.output.setValue( outfile )
 
-        self.show_status("starting workflow", 100)
+        self.show_status("workflow ... done", 100)
+
+class GenerateAndRun(WPSProcess):
+    def __init__(self):
+        WPSProcess.__init__(self,
+            identifier = "restflow_generate_and_run",
+            title = "Generate and Run Restflow",
+            version = "0.1",
+            metadata=[
+                {"title":"Restflow","href":"https://github.com/restflow-org"},
+                ],
+            abstract="Generates Restflow workflow description in yaml from given json input parameters and starts workflow.")
+
+        self.name = self.addLiteralInput(
+            identifier="name",
+            title="Workflow",
+            abstract="Choose Workflow",
+            default="simpleWorkflow",
+            type=type(''),
+            minOccurs=1,
+            maxOccurs=1,
+            allowedValues=['simpleWorkflow']
+            )
+
+        self.nodes= self.addComplexInput(
+            identifier="nodes",
+            title="Workflow Nodes",
+            abstract="Workflow Nodes in JSON",
+            metadata=[],
+            minOccurs=1,
+            maxOccurs=1,
+            formats=[{"mimeType":"text/json"}],
+            maxmegabites=2
+            )
+        
+        self.output = self.addComplexOutput(
+            identifier="output",
+            title="Workflow Result",
+            abstract="Workflow Result",
+            formats=[{"mimeType":"application/json"}],
+            asReference=True,
+            )
+       
+    def execute(self):
+        self.show_status("Generate workflow ...", 0)
+
+        # TODO: handle multiple values (fix in pywps)
+        # http://pymotw.com/2/json/
+        nodes = None
+        with open(self.nodes.getValue()) as fp:
+            import yaml
+            # TODO: fix json encode to unicode
+            nodes = yaml.load(fp)
+        wf_yaml = restflow.generate(self.name.getValue(), nodes)
+
+        self.show_status("Starting workflow ...", 5)
+
+        status = lambda msg, percent: self.show_status(msg, percent)
+        result = restflow.run(
+            wf_yaml, 
+            timeout=config.timeout(), 
+            status_callback=status)
+
+        import json
+        outfile = self.mktempfile(suffix='.json')
+        with open(outfile, 'w') as fp:
+            json.dump(obj=result, fp=fp, indent=4, sort_keys=True)
+        self.output.setValue( outfile )
+
+        self.show_status("workflow ... done", 100)
 
 
