3f0d5cb5129badf7bed172aeca3ab72fad5dc5c5
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-10-31 14:12:29 +0100

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo "Starting bootstrap"
-python -c 'import urllib; print urllib.urlopen("https://raw.githubusercontent.com/bird-house/birdhousebuilder.bootstrap/master/install.sh").read()' > install.sh
+python -c 'import urllib; print urllib.urlopen("https://raw.githubusercontent.com/bird-house/birdhousebuilder.bootstrap/master/Makefile").read()' > Makefile
 echo "Updated installation script"
-bash install.sh bootstrap
 echo "Bootstrap done"
 
