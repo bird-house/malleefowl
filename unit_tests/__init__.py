a5b68da50868cfae900a2237f546ce67900b740b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-11-28 18:16:03 +0100

    load testdata for unit_tests

--- unit_tests/__init__.py
+++ unit_tests/__init__.py
@@ -1,3 +1,16 @@
+import logging
+
 SERVICE = "http://localhost:8091/wps"
-TEST1_NC = "http://localhost:8091/files/test_malleefowl.org/test1.nc"
-TEST2_NC = "http://localhost:8091/files/test_malleefowl.org/test2.nc"
+TESTDATA = {}
+
+
+from os.path import join, dirname
+__testdata_filename__ = join(dirname(__file__), 'testdata.json')
+
+try:
+    from os.path import join
+    import json
+    with open(__testdata_filename__, 'r') as fp:
+        TESTDATA = json.load(fp)
+except:
+    logging.error('could not read testdata! %s', __testdata_filename__ )
---
+++ unit_tests/test_fetch.py
@@ -0,0 +1,10 @@
+import nose.tools
+from nose import SkipTest
+from nose.plugins.attrib import attr
+
+from __init__ import TESTDATA
+
+def test_testdata():
+    nose.tools.ok_( len(TESTDATA) > 0, TESTDATA )
+    nose.tools.ok_( 'file:///' in TESTDATA.values()[0], TESTDATA)
+    #nose.tools.ok_(False, TESTDATA)
