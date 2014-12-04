5fe330b55a382c36232204e61c35160704e22216
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-12-04 12:50:52 +0100

    working on esgsearch workflow

--- malleefowl/dispel.py
+++ malleefowl/dispel.py
@@ -4,22 +4,35 @@ from malleefowl import wpslogging as logging
 logger = logging.getLogger(__name__)
 
 class WebProcessingServicePE(GenericPE):
-    def __init__(self, url, identifier, inputs, output='output'):
+    def __init__(self, url, identifier, resource='resource', inputs=[], output='output'):
         GenericPE.__init__(self)
         from owslib.wps import WebProcessingService
         self.wps = WebProcessingService(url)
         self.identifier = identifier
-        self.inputs = inputs
-        self.output = output
+        self.wps_resource = resource
+        self.wps_inputs = inputs
+        self.wps_output = output
         self.inputconnections['resource'] = { NAME : 'resource' }
+        self.inputconnections['resource_url'] = { NAME : 'resource_url' }
         self.outputconnections['output'] = { NAME : 'output'}
     
     def process(self, inputs):
         from owslib.wps import monitorExecution
+        #print inputs.keys()
+        #print inputs.values()
+        if inputs.has_key('resource'):
+            for value in inputs['resource']:
+                self.wps_inputs.append((self.wps_resource, str(value)))
+        elif inputs.has_key('resource_url'):
+            import json
+            import urllib2
+            values = json.load(urllib2.urlopen(inputs['resource_url'][0]))
+            for value in values:
+                self.wps_inputs.append((self.wps_resource, str(value)))
         execution = self.wps.execute(
             identifier=self.identifier,
-            inputs=self.inputs,
-            output=[(self.output, True)])
+            inputs=self.wps_inputs,
+            output=[(self.wps_output, True)])
         monitorExecution(execution)
         outputs = { 'output' : [execution.processOutputs[0].reference] }
         return outputs
--- unit_tests/test_dispel4py.py
+++ unit_tests/test_dispel4py.py
@@ -1,22 +1,35 @@
 from dispel4py.workflow_graph import WorkflowGraph
 from dispel4py import simple_process
-from dispel4py.examples.graph_testing.testing_PEs import TestOneInOneOut
 
 from malleefowl.dispel import WebProcessingServicePE
 
 from nose import tools
 
-from __init__ import SERVICE
+from __init__ import SERVICE, TESTDATA
 
 def test_esgsearch():
     graph = WorkflowGraph()
+    print 'before esgsearch'
     esgsearch = WebProcessingServicePE(
         url=SERVICE,
         identifier='esgsearch',
         inputs=[('constraints', 'project:CORDEX')]
         )
-    donothing = TestOneInOneOut()
-    graph.connect(esgsearch, 'output', donothing, 'input')
+    print 'before download'
+    download = WebProcessingServicePE(
+        url=SERVICE,
+        identifier='wget',
+        inputs=[('resource',
+                 'http://localhost:8090/wpscache/tasmax_WAS-44_MPI-M-MPI-ESM-LR_historical_r1i1p1_MPI-CSC-REMO2009_v1_day_20010101-20051231.nc' )]
+        )
+    print 'before doit'
+    doit = WebProcessingServicePE(
+        url='http://localhost:8092/wps',
+        identifier='cdo_sinfo',
+        resource='netcdf_file',
+        )
+    graph.connect(esgsearch, 'output', download, 'ignore')
+    graph.connect(download, 'output', doit, 'resource_url')
     results = simple_process.process(graph, {esgsearch: [{}]})
     tools.ok_(False, results)
     
