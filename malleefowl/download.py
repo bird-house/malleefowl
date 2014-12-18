8ccf73b09c2f7310add830795a2ad0e6e65b5e66
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-12-18 16:08:57 +0100

    moving wget code to download.py

--- malleefowl/download.py
+++ malleefowl/download.py
@@ -17,4 +17,45 @@ def download(url, cache_enabled=True):
         filename = wget.download(url, out=config.cache_path(), bar=None)
     return filename
 
+def wget_download_with_archive(url, credentials=None):
+    from .utils import esgf_archive_path
+    local_url = esgf_archive_path(url)
+    if local_url is None:
+        local_url = wget_download(url, credentials)
+    return local_url
+
+def wget_download(url, credentials=None):
+    from os.path import basename
+    resource_name = basename(url)
+    logger.debug('downloading %s', url)
+
+    from subprocess import check_call
+    try:
+        cmd = ["wget"]
+        if credentials is not None:
+            logger.debug('using credentials')
+            cmd.append("--certificate")
+            cmd.append(credentials) 
+            cmd.append("--private-key")
+            cmd.append(credentials)
+        cmd.append("--no-check-certificate") 
+        cmd.append("-N")
+        cmd.append("-P")
+        cmd.append(config.cache_path())
+        cmd.append("--progress")
+        cmd.append("dot:mega")
+        cmd.append(url)
+        check_call(cmd)
+    except:
+        msg = "wget failed on %s. Maybe not authorized? " % (resource_name)
+        logger.exception(msg)
+        raise Exception(msg)
+
+    from os.path import join
+    cached_file = join(config.cache_path(), resource_name)
+    local_url = "file://" + cached_file
+    #external_url = config.cache_url() + '/' + resource_name
+
+    return local_url
+
 
--- malleefowl/processes/wget.py
+++ malleefowl/processes/wget.py
@@ -1,7 +1,6 @@
 from malleefowl.process import WPSProcess
 from malleefowl import config
-
-from malleefowl.utils import esgf_archive_path
+from malleefowl.download import wget_download_with_archive
 
 from malleefowl import wpslogging as logging
 logger = logging.getLogger(__name__)
@@ -42,79 +41,25 @@ class Wget(WPSProcess):
             asReference=True,
             )
 
-        #self.output_external = self.addComplexOutput(
-        #    identifier="output_external",
-        #    title="Downloaded files for external access",
-        #    abstract="Json document with list of downloaded files with http url for external use.",
-        #    metadata=[],
-        #   formats=[{"mimeType":"test/json"}],
-        #    asReference=True,
-        #    )
-
-    def _wget(self, url, credentials=None):
-        from os.path import basename
-        resource_name = basename(url)
-        logger.debug('downloading %s', url)
-        #logger.debug('credentials %s', credentials)
-        
-        try:
-            cmd = ["wget"]
-            if credentials is not None:
-                logger.debug('using credentials')
-                cmd.append("--certificate")
-                cmd.append(credentials) 
-                cmd.append("--private-key")
-                cmd.append(credentials)
-            cmd.append("--no-check-certificate") 
-            cmd.append("-N")
-            cmd.append("-P")
-            cmd.append(config.cache_path())
-            cmd.append("--progress")
-            cmd.append("dot:mega")
-            cmd.append(url)
-            self.cmd(cmd, stdout=True)
-        except Exception, e:
-            msg = "wget failed on %s. Maybe not authorized? " % (resource_name)
-            logger.exception(msg)
-            raise Exception(msg + str(e))
-
-        from os.path import join
-        cached_file = join(config.cache_path(), resource_name)
-        local_url = "file://" + cached_file
-        #external_url = config.cache_url() + '/' + resource_name
-
-        return local_url
-
     def execute(self):
         self.show_status("Downloading ...", 0)
 
         credentials = self.credentials.getValue()
-        resource = self.resource.getValue()
-
-        logger.debug('credentials: %s', credentials)
-
-        import types
-        if type(resource) != types.ListType:
-            resource = [resource]
+        resources = self.getInputValues(identifier='resource')
 
         local_files = []
-        #external_files = []
 
         count = 0
-        max_count = len(resource)
-        for url in resource:
+        max_count = len(resources)
+        for url in resources:
             count = count + 1
             progress = (count-1) * 100.0 / max_count
             self.show_status("Downloading %d/%d" % (count, max_count), progress)
 
-            #external_url = None
-            local_url = esgf_archive_path(url)
-            if local_url is None:
-                local_url = self._wget(url, credentials=credentials)
-            
-            local_files.append(local_url)
-            #if external_url is not None:
-            #    external_files.append(external_url)
+            try:
+                local_files.append(wget_download_with_archive(url, credentials))
+            except:
+                logger.exception("Failed to download %s", url)
 
         if max_count > len(local_files):
             logger.warn('Could not retrieve all files: %d from %d', len(local_files), max_count)
@@ -126,11 +71,6 @@ class Wget(WPSProcess):
         with open(outfile, 'w') as fp:
             json.dump(obj=local_files, fp=fp, indent=4, sort_keys=True)
         self.output.setValue( outfile )
-
-        #outfile = self.mktempfile(suffix='.json')
-        #with open(outfile, 'w') as fp:
-        #    json.dump(obj=external_files, fp=fp, indent=4, sort_keys=True)
-        #self.output_external.setValue( outfile )
 
         self.show_status("Downloading ... done", 100)
 
