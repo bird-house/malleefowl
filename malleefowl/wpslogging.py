a726e33d202212f03c5da5095fbd4e75a8f3bc1b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-12-22 18:23:38 +0100

    cleaned up wpslogging

--- malleefowl/wpslogging.py
+++ malleefowl/wpslogging.py
@@ -1,23 +1,18 @@
 import logging
 
 # https://pypi.python.org/pypi/ConcurrentLogHandler/0.8.3
-from cloghandler import ConcurrentRotatingFileHandler
+try:
+    from cloghandler import ConcurrentRotatingFileHandler as RFHandler
+except ImportError:
+    # Next 2 lines are optional:  issue a warning to the user
+    from warnings import warn
+    warn("ConcurrentLogHandler package not installed.  Using builtin log handler")
+    from logging.handlers import RotatingFileHandler as RFHandler
+
 
 @property
 def DEBUG():
     return logging.DEBUG
-
-class TraceLogger(object):
-    def __init__(self, filename="trace.log"):
-        self.logger = open(filename, "a")
-
-    def write(self, message):
-        import datetime 
-        log_message = '{message}'.format(
-            #asctime=str(datetime.datetime.now())[:19],
-            message=message)
-        self.logger.write(log_message + '\n')
-        self.logger.flush()
 
 def getLogger(name):
     from pywps import config as wpsconfig
@@ -34,24 +29,8 @@ def getLogger(name):
 
     formatter = logging.Formatter('%(asctime)-15s - %(process)d - %(levelname)-8s %(message)s')
 
-    # log stdout to trace
-    #import sys
-    #sys.stdout = TraceLogger(os.path.join(log_path, 'malleefowl_trace.log'))
-    
-    # warn
-    #fh = logging.StreamHandler(stream=sys.stdout) #os.path.join(log_path, 'malleefowl_warn.log'))
-    #fh.setLevel(logging.WARN)
-    #fh.setFormatter(formatter)
-    #logger.addHandler(fh)
-
-    # info
-    #fh = logging.StreamHandler(stream=sys.stdout) #os.path.join(log_path, 'malleefowl_info.log'))
-    #fh.setLevel(logging.INFO)
-    #fh.setFormatter(formatter)
-    #logger.addHandler(fh)
-
-    # debug
-    fh = ConcurrentRotatingFileHandler(log_file, "a", 512*1024, 5) 
+    # log handler
+    fh = RFHandler(log_file, "a", 2*1024*1024, 5) 
     fh.setLevel(logging.DEBUG)
     fh.setFormatter(formatter)
     logger.addHandler(fh)
