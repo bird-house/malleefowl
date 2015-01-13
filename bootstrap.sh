bf6d8ee4136cf4d532e77007081ecd1555628789
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-01-13 15:06:02 +0100

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,10 +21,10 @@ install_pkgs() {
     elif [ -f /etc/redhat-release ] ; then
         echo "Install CentOS packages for Birdhouse build ..."
         #sudo rpm -i http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-        sudo yum -y update
-        sudo yum -y install epel-release
-        sudo yum -y install wget curl gcc-c++
-        sudo yum -y install vim-common  # anaconda needs xxd
+        sudo yum update -y
+        sudo yum install -y epel-release
+        sudo yum install -y wget curl gcc-c++
+        sudo yum install -y vim-common  # anaconda needs xxd
     elif [ `uname -s` = "Darwin" ] ; then
         echo "Install Homebrew packages for Birdhouse build ..."
         brew install wget curl libmagic
