091c0faa1e513d14c913af7142b443e856eac72f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-04-14 18:09:30 +0200

    process "cloud login" added

--- malleefowl/processes/wps_cloud.py
+++ malleefowl/processes/wps_cloud.py
@@ -4,6 +4,50 @@ from malleefowl import cloud
 from malleefowl import wpslogging as logging
 logger = logging.getLogger(__name__)
 
+class Login(WPSProcess):
+    def __init__(self):
+        WPSProcess.__init__(self,
+            identifier = "cloud_login",
+            title = "Login to Swift Cloud",
+            version = "1.0",
+            abstract="Login to Swift Cloud and get Token.")
+
+        self.username = self.addLiteralInput(
+            identifier="username",
+            title="Username",
+            minOccurs=1,
+            maxOccurs=1,
+            type=type(''),
+            )
+
+        self.password = self.addLiteralInput(
+            identifier = "password",
+            title = "Password",
+            minOccurs=1,
+            maxOccurs=1,
+            type=type(''),
+            )
+
+        self.auth_token = self.addLiteralOutput(
+            identifier="auth_token",
+            title="Auth Token",
+            type=type(''),
+            )
+
+        self.storage_url = self.addLiteralOutput(
+            identifier="storage_url",
+            title="Storage URL",
+            type=type(''),
+            )
+
+    def execute(self):
+        (storage_url, auth_token) = cloud.login(
+            self.username.getValue(),
+            self.password.getValue())
+
+        self.storage_url.setValue( storage_url )
+        self.auth_token.setValue( auth_token )
+
 class Download(WPSProcess):
     def __init__(self):
         WPSProcess.__init__(self,
@@ -15,16 +59,14 @@ class Download(WPSProcess):
         self.storage_url = self.addLiteralInput(
             identifier="storage_url",
             title="Storage URL",
-            abstract="Storage URL",
             minOccurs=1,
             maxOccurs=1,
-            type=type('')
+            type=type(''),
             )
 
         self.auth_token = self.addLiteralInput(
             identifier = "auth_token",
             title = "Auth Token",
-            abstract = "Auth Token",
             minOccurs=1,
             maxOccurs=1,
             type=type(''),
