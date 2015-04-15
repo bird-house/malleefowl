29b596d403a51bd7fc3791043617d8b07ccf8790
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-04-15 14:04:11 +0200

    process for cloud.download_urls added

--- malleefowl/processes/wps_cloud.py
+++ malleefowl/processes/wps_cloud.py
@@ -103,3 +103,58 @@ class Download(WPSProcess):
         self.output.setValue( outfile )
 
 
+class DownloadUrls(WPSProcess):
+    def __init__(self):
+        WPSProcess.__init__(self,
+            identifier = "cloud_download_urls",
+            title = "Provide download URLs for files from Swift Cloud",
+            version = "1.0",
+            abstract="Provide download URLs for files from Swift Cloud and return url list as json document.")
+
+        self.storage_url = self.addLiteralInput(
+            identifier="storage_url",
+            title="Storage URL",
+            minOccurs=1,
+            maxOccurs=1,
+            type=type(''),
+            )
+
+        self.auth_token = self.addLiteralInput(
+            identifier = "auth_token",
+            title = "Auth Token",
+            minOccurs=1,
+            maxOccurs=1,
+            type=type(''),
+            )
+
+        self.container = self.addLiteralInput(
+            identifier = "container",
+            title = "Container",
+            abstract = "Container",
+            minOccurs=1,
+            maxOccurs=1,
+            type=type(''),
+            )
+
+        self.output = self.addComplexOutput(
+            identifier="output",
+            title="Downloaded files",
+            abstract="JSON document with list of downloaded files with file url.",
+            metadata=[],
+            formats=[{"mimeType":"test/json"}],
+            asReference=True,
+            )
+
+    def execute(self):
+        files = cloud.download_urls(
+            self.storage_url.getValue(),
+            self.auth_token.getValue(),
+            self.container.getValue())
+
+        import json
+        outfile = self.mktempfile(suffix='.json')
+        with open(outfile, 'w') as fp:
+            json.dump(obj=files, fp=fp, indent=4, sort_keys=True)
+        self.output.setValue( outfile )
+
+
