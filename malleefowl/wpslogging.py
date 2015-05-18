4610246147b3937a2dc4d345adc1f0d25c24c46b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-05-18 14:04:30 +0200

    updated supervisor, logging to stdout/supervisor

--- malleefowl/wpslogging.py
+++ malleefowl/wpslogging.py
@@ -1,26 +1,15 @@
 import logging
 
-# https://pypi.python.org/pypi/ConcurrentLogHandler/0.8.3
-try:
-    from cloghandler import ConcurrentRotatingFileHandler as RFHandler
-except ImportError:
-    # Next 2 lines are optional:  issue a warning to the user
-    from warnings import warn
-    warn("ConcurrentLogHandler package not installed.  Using builtin log handler")
-    from logging.handlers import RotatingFileHandler as RFHandler
-
-
 @property
 def DEBUG():
     return logging.DEBUG
 
 def getLogger(name):
+    # get log level from wps config
     from pywps import config as wpsconfig
-    log_file = wpsconfig.getConfigValue("server", "logFile").replace('.log', '_trace.log')
-    if log_file == '':
-        log_file = "pywps_trace.log"
     log_level = wpsconfig.getConfigValue("server", "logLevel")
-    
+
+    # create logger
     logger = logging.getLogger(name)
     if 'DEBUG' in log_level:
         logger.setLevel(logging.DEBUG)
@@ -30,13 +19,17 @@ def getLogger(name):
     # https://urllib3.readthedocs.org/en/latest/security.html#insecureplatformwarning
     logging.captureWarnings(True)
 
-    formatter = logging.Formatter('%(asctime)-15s - %(process)d - %(levelname)-8s %(message)s')
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
 
-    # log handler
-    fh = RFHandler(log_file, "a", 2*1024*1024, 5) 
-    fh.setLevel(logging.DEBUG)
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
+    # create formatter
+    formatter = logging.Formatter('%(asctime)-15s - %(process)s - %(levelname)-8s - %(message)s')
 
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
     return logger
     
