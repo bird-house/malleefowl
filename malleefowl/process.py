2f36d75a4e62dd5c162bdbe79dde534892ef93f4
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-05-20 15:08:09 +0200

    cleaned up wpsprocess base class

--- malleefowl/process.py
+++ malleefowl/process.py
@@ -1,7 +1,3 @@
-import os
-import types
-import tempfile
-
 from pywps.Process import WPSProcess as PyWPSProcess
 
 from malleefowl import config
@@ -10,13 +6,9 @@ from malleefowl import wpslogging as logging
 logger = logging.getLogger(__name__)
 
 class WPSProcess(PyWPSProcess):
-    """This is the base class for all climdaps wps processes."""
+    """This is the base class for wps processes with some convenient functions."""
 
     def __init__(self, identifier, title, version, metadata=[], abstract=""):
-
-        # TODO: fix metadata appending
-        #metadata.append({"title":"Hardworking Bird Malleefowl", "href":"http://en.wikipedia.org/wiki/Malleefowl"})
-
         PyWPSProcess.__init__(
             self,
             identifier = identifier,
@@ -25,26 +17,25 @@ class WPSProcess(PyWPSProcess):
             storeSupported = "true",   # async
             statusSupported = "true",  # retrieve status, needs to be true for async 
             metadata = metadata,
-            abstract=abstract,
+            abstract = abstract,
             grassLocation = False)
 
     @property
     def working_dir(self):
+        import os
         return os.path.abspath(os.curdir)
 
     def mktempfile(self, suffix='.txt'):
+        import tempfile
         (_, filename) = tempfile.mkstemp(dir=self.working_dir, suffix=suffix)
         return filename
-
-    def sleep(self, secs):
-        import time
-        time.sleep(secs)
 
     def show_status(self, msg, percent_done):
         logger.info('STATUS (%d/100) - %s: %s', percent_done, self.identifier, msg)
         self.status.set(msg=msg, percentDone=percent_done, propagate=True)
 
     def getInputValues(self, identifier):
+        import types
         values = self.getInputValue(identifier)
         if values is None:
             values = []
