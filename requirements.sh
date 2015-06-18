8067f1c9286012ff5eb31290733b6bb8bf928166
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-06-18 13:01:00 +0200

    disabled myproxy install in reqirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -f /etc/debian_version ] ; then
-    sudo apt-get -y --force-yes install myproxy
+    #sudo apt-get -y --force-yes install myproxy
     # java
     sudo apt-get install -y --force-yes openjdk-7-jre openjdk-7-jdk
     if [ -d /usr/lib/jvm/java-7-openjdk-amd64 ] ; then
@@ -9,14 +9,12 @@ if [ -f /etc/debian_version ] ; then
         sudo update-alternatives --set java /usr/lib/jvm/java-7-openjdk-i386/jre/bin/java
     fi
 elif [ -f /etc/redhat-release ] ; then
-    sudo yum -y install myproxy
-    # for pyopenssl 0.13.1
-    #sudo yum -y install openssl openssl-devel
+    #sudo yum -y install myproxy
     # java
     sudo yum install -y java-1.7.0-openjdk java-1.7.0-openjdk-devel 
 elif [ `uname -s` = "Darwin" ] ; then
     # java
     brew cask install java
     # globus myproxy
-    brew install globus-toolkit
+    #brew install globus-toolkit
 fi
