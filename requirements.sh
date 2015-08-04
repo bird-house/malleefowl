8b22f2de897891e24f34416684d7d4ed31294885
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-08-04 14:36:08 +0200

    removed java from system requirements

--- requirements.sh
+++ requirements.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 if [ -f /etc/debian_version ] ; then
-    #sudo apt-get -y --force-yes install myproxy
-    # java
-    sudo apt-get install -y --force-yes openjdk-7-jre openjdk-7-jdk
-    if [ -d /usr/lib/jvm/java-7-openjdk-amd64 ] ; then
-        sudo update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
-    elif [ -d /usr/lib/jvm/java-7-openjdk-i386 ] ; then
-        sudo update-alternatives --set java /usr/lib/jvm/java-7-openjdk-i386/jre/bin/java
-    fi
+    echo "Installing Debian/Ubuntu packages ..."
+    #sudo apt-get install -y --force-yes openjdk-7-jre openjdk-7-jdk
+    #if [ -d /usr/lib/jvm/java-7-openjdk-amd64 ] ; then
+    #    sudo update-alteratives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
+    #elif [ -d /usr/lib/jvm/java-7-openjdk-i386 ] ; then
+    #    sudo update-alternatives --set java /usr/lib/jvm/java-7-openjdk-i386/jre/bin/java
+    #fi
 elif [ -f /etc/redhat-release ] ; then
-    #sudo yum -y install myproxy
+    echo "Installing RedHat/CentOS packages ..."
     # java
-    sudo yum install -y java-1.7.0-openjdk java-1.7.0-openjdk-devel 
+    #sudo yum install -y java-1.7.0-openjdk java-1.7.0-openjdk-devel 
 elif [ `uname -s` = "Darwin" ] ; then
+    echo "Installing MacOSX/homebrew packages ..."
     # java
-    brew cask install java
-    # globus myproxy
-    #brew install globus-toolkit
+    #brew cask install java
 fi
