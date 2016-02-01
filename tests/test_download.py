bcca0c5137c920849ccedc7e1469b210d76ac23f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-01 15:07:37 +0100

    refacted test_download

--- tests/test_download.py
+++ tests/test_download.py
@@ -3,32 +3,25 @@ from unittest import TestCase
 from nose import SkipTest
 from nose.plugins.attrib import attr
 
-from os.path import basename
-
+import os
 from malleefowl.download import download, download_with_archive, download_files
+from tests.common import TESTDATA
 
 class DownloadTestCase(TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.slp_1955_nc = "http://www.esrl.noaa.gov/psd/thredds/fileServer/Datasets/ncep.reanalysis.dailyavgs/surface/slp.1955.nc"
-        
     @attr('online')
-    def test_wget_download(self):
-        result = download(self.slp_1955_nc)
-        nose.tools.ok_(basename(result) == "slp.1955.nc", result)
+    def test_download(self):
+        result = download(TESTDATA['noaa_nc_1'])
+        nose.tools.ok_(os.path.basename(result) == "slp.1955.nc", result)
         nose.tools.ok_(not 'file:///' in result, result)
 
     @attr('online')
-    def test_wget_download_with_file_url(self):
-        result = download(self.slp_1955_nc, use_file_url=True)
-        nose.tools.ok_(basename(result) == "slp.1955.nc", result)
+    def test_download_with_file_url(self):
+        result = download(TESTDATA['noaa_nc_1'], use_file_url=True)
+        nose.tools.ok_(os.path.basename(result) == "slp.1955.nc", result)
         nose.tools.ok_('file:///' in result, result)
 
-    @attr('online')
-    def test_download_files(self):
-        results = download_files(urls=[self.slp_1955_nc])
-        nose.tools.ok_(len(results) == 1, results)
+
         
         
 
