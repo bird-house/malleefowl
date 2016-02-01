7c988db08b07ed2aeb5a46ca7aae2cb20236a3cd
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-01 15:14:18 +0100

    add test wps_download

--- tests/test_wps_download.py
+++ tests/test_wps_download.py
@@ -1,13 +1,12 @@
 import nose.tools
-from nose import SkipTest
 from nose.plugins.attrib import attr
 
 from tests.common import WpsTestClient, TESTDATA, assert_response_success
 
 @attr('online')
-def test_wps_thredds_download():
+def test_wps_download():
     wps = WpsTestClient()
-    datainputs = "[url={0}]".format(TESTDATA['noaa_catalog_1'])
-    resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='thredds_download',
+    datainputs = "[resource={0}]".format(TESTDATA['noaa_catalog_1'])
+    resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='download',
                    datainputs=datainputs)
     assert_response_success(resp)
