716db1620e64ab64464ce7c3c4dbaceebc59fc8e
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-02-01 15:19:37 +0100

    fixed esgf logon test

--- tests/test_esgf_logon.py
+++ tests/test_esgf_logon.py
@@ -1,13 +1,13 @@
 import nose.tools
-from nose import SkipTest
+from nose.plugins.attrib import attr
 
 from malleefowl.esgf import logon
 
-def test_parse():
-    raise SkipTest
-    openid="https://esgf-data.dkrz.de/esgf-idp/openid/pingutest"
-    (username, hostname, port) = logon.parse(openid)
-    nose.tools.ok_( username == "pingutest", username)
+@attr('online')
+def test_parse_openid():
+    openid="https://esgf-data.dkrz.de/esgf-idp/openid/pingutest1"
+    (username, hostname, port) = logon.parse_openid(openid)
+    nose.tools.ok_( username == "pingutest1", username)
     nose.tools.ok_( hostname == "esgf-data.dkrz.de", hostname)
     nose.tools.ok_( port == "7512", port)
 
