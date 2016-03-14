e37267bbf7ba1e50141d1e507f344b2a4adc26cd
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-03-14 18:09:56 +0100

    skip thredds test

--- tests/test_wps_thredds.py
+++ tests/test_wps_thredds.py
@@ -1,10 +1,12 @@
 import nose.tools
+from nose import SkipTest
 from nose.plugins.attrib import attr
 
 from tests.common import WpsTestClient, TESTDATA, assert_response_success
 
 @attr('online')
 def test_wps_thredds_download():
+    raise SkipTest
     wps = WpsTestClient()
     datainputs = "[url={0}]".format(TESTDATA['noaa_catalog_1'])
     resp = wps.get(service='wps', request='execute', version='1.0.0', identifier='thredds_download',
