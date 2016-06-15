c7308559233d99a79baea5571e502e3c47f0472e
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-06-15 16:47:22 +0200

    release 0.3.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '0.3.11'
+version = '0.3.12'
 description = 'Malleefowl simplifies the usage of WPS and has some supporting WPS processes.'
 long_description = (
     open('README.rst').read() + '\n' +
