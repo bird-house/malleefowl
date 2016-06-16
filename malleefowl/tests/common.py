5c9192308dda06d44d68704ebba961275af2d9ea
Mac Pingu <ehbrecht@dkrz.de>
2016-06-16 23:18:24 +0200

    update birdhouse home for tests

--- malleefowl/tests/common.py
+++ malleefowl/tests/common.py
@@ -22,7 +22,7 @@ TESTDATA = {
 class WpsTestClient(object):
     def __init__(self):
         pywps_path = os.path.dirname(pywps.__file__)
-        os.environ['PYWPS_CFG'] = os.path.abspath(os.path.join(pywps_path, '..', '..', '..', '..', 'etc', 'pywps', 'malleefowl.cfg'))
+        os.environ['PYWPS_CFG'] = os.path.abspath(os.path.join(os.environ['HOME'], 'birdhouse', 'etc', 'pywps', 'malleefowl.cfg'))
         os.environ['REQUEST_METHOD'] = pywps.METHOD_GET
         self.wps = pywps.Pywps(os.environ["REQUEST_METHOD"], os.environ.get("PYWPS_CFG"))
    
