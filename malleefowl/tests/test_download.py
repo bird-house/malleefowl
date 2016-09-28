7a598b07e6c6f2c9bfaecb3a5e55dc72176ced70
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-09-28 13:08:11 +0200

    pep8

--- malleefowl/tests/test_download.py
+++ malleefowl/tests/test_download.py
@@ -5,6 +5,7 @@ import os
 from malleefowl.download import download, download_with_archive, download_files
 from malleefowl.tests.common import TESTDATA
 
+
 class DownloadTestCase(TestCase):
 
     @pytest.mark.online
@@ -18,8 +19,3 @@ class DownloadTestCase(TestCase):
         result = download(TESTDATA['noaa_nc_1'], use_file_url=True)
         assert os.path.basename(result) == "slp.1955.nc"
         assert 'file:///' in result
-
-
-        
-        
-
