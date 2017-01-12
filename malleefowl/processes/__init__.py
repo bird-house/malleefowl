de3c04f4c59e3c168d662c762de0402ac6994cdf
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-12 14:08:16 +0100

    converted esgsearch process to pywps 4.x

--- malleefowl/processes/__init__.py
+++ malleefowl/processes/__init__.py
@@ -1,4 +1,5 @@
 from .wps_esgflogon import MyProxyLogon
+from .wps_esgsearch import ESGSearchProcess
 from .wps_download import Download
 from .wps_thredds import ThreddsDownload
 from .wps_workflow import DispelWorkflow
@@ -6,6 +7,7 @@ from .wps_workflow import DummyProcess
 
 processes = [
     MyProxyLogon(),
+    ESGSearchProcess(),
     Download(),
     ThreddsDownload(),
     DispelWorkflow(),
--- malleefowl/processes/wps_esgsearch.py
+++ malleefowl/processes/wps_esgsearch.py
@@ -1,207 +1,176 @@
 import json
-from pywps.Process import WPSProcess
-
 from datetime import datetime
 from dateutil import parser as date_parser
+
+from pywps import Process
+from pywps import LiteralInput
+from pywps import ComplexInput
+from pywps import LiteralOutput
+from pywps import ComplexOutput
+from pywps import Format, FORMATS
+from pywps.app.Common import Metadata
 
 from malleefowl.esgf.search import ESGSearch
 
 
-class ESGSearchProcess(WPSProcess):
+class ESGSearchProcess(Process):
     """
     wps wrapper for esg search.
 
     TODO: bbox constraint for datasets
     """
     def __init__(self):
-        WPSProcess.__init__(
-            self,
+        inputs = [
+            LiteralInput('url', 'URL',
+                         data_type='string',
+                         abstract="URL of ESGF Search Index. Example: http://esgf-data.dkrz.de/esg-search",
+                         min_occurs=1,
+                         max_occurs=1,
+                         ),
+            LiteralInput('distrib', 'Distributed',
+                         data_type='boolean',
+                         abstract="If flag is set then a distributed search will be run.",
+                         min_occurs=1,
+                         max_occurs=1,
+                         default=False,
+                         ),
+            LiteralInput('replica', 'Replica',
+                         data_type='boolean',
+                         abstract="If flag is set then search will include replicated datasets.",
+                         min_occurs=1,
+                         max_occurs=1,
+                         default=False,
+                         ),
+            LiteralInput('latest', 'Latest',
+                         data_type='boolean',
+                         abstract="If flag is set then search will include only latest datasets.",
+                         min_occurs=1,
+                         max_occurs=1,
+                         default=True,
+                         ),
+            LiteralInput('temporal', 'Temporal',
+                         data_type='boolean',
+                         abstract="If flag is set then search will use temporal filter.",
+                         min_occurs=1,
+                         max_occurs=1,
+                         default=True,
+                         ),
+            LiteralInput('search_type', 'Search Type',
+                         data_type='string',
+                         abstract="Search on Datasets, Files or Aggregations.",
+                         min_occurs=1,
+                         max_occurs=1,
+                         default='Dataset',
+                         allowed_values=['Dataset', 'File', 'Aggregation']
+                         ),
+            LiteralInput('constraints', 'Constraints',
+                         data_type='string',
+                         abstract="Constraints as list of key/value pairs."
+                                  "Example: project:CORDEX, time_frequency:mon, variable:tas",
+                         min_occurs=1,
+                         max_occurs=1,
+                         default="project:CORDEX, time_frequency:mon, variable:tas",
+                         ),
+            LiteralInput('query', 'Query',
+                         data_type='string',
+                         abstract="Freetext query. For Example: temperatue",
+                         min_occurs=0,
+                         max_occurs=1,
+                         default='*',
+                         ),
+            LiteralInput('start', 'Start',
+                         data_type='dateTime',
+                         abstract="Startime: 2000-01-11T12:00:00Z",
+                         min_occurs=0,
+                         max_occurs=1,
+                         ),
+            LiteralInput('end', 'End',
+                         data_type='dateTime',
+                         abstract="Endtime: 2005-12-31T12:00:00Z",
+                         min_occurs=0,
+                         max_occurs=1,
+                         ),
+            LiteralInput('limit', 'Limit',
+                         data_type='integer',
+                         abstract="Maximum number of datasets in search result",
+                         min_occurs=1,
+                         max_occurs=1,
+                         default=10,
+                         allowed_values=[0, 1, 2, 5, 10, 20, 50, 100, 200]
+                         ),
+            LiteralInput('offset', 'Offset',
+                         data_type='integer',
+                         abstract="Start search of datasets at offset.",
+                         min_occurs=1,
+                         max_occurs=1,
+                         default=0,
+                         ),
+        ]
+        outputs = [
+            ComplexOutput('output', 'Search Result',
+                          abstract="JSON document with search result",
+                          as_reference=True,
+                          supported_formats=[Format('application/json')]),
+            ComplexOutput('summary', 'Search Result Summary',
+                          abstract="JSON document with search result summary",
+                          as_reference=True,
+                          supported_formats=[Format('application/json')]),
+            ComplexOutput('facet_counts', 'Facet Counts',
+                          abstract="JSON document with facet counts for constraints.",
+                          as_reference=True,
+                          supported_formats=[Format('application/json')]),
+
+        ]
+
+        super(ESGSearchProcess, self).__init__(
+            self._handler,
             identifier="esgsearch",
             title="ESGF Search",
-            version="0.5",
+            version="0.6",
             abstract="Search ESGF datasets, files and aggreations.",
-            statusSupported=True,
-            storeSupported=True)
-
-        self.url = self.addLiteralInput(
-            identifier="url",
-            title="URL",
-            abstract="URL of ESGF Search Index. Example: http://esgf-data.dkrz.de/esg-search",
-            default='https://esgf-data.dkrz.de/esg-search',
-            minOccurs=1,
-            maxOccurs=1,
-            type=type('')
+            metadata=[
+                Metadata('Birdhouse', 'http://bird-house.github.io/'),
+                Metadata('User Guide', 'http://malleefowl.readthedocs.io/en/latest/'),
+            ],
+            inputs=inputs,
+            outputs=outputs,
+            status_supported=True,
+            store_supported=True,
         )
 
-        self.distrib = self.addLiteralInput(
-            identifier="distrib",
-            title="Distributed",
-            abstract="If flag is set then a distributed search will be run.",
-            default=False,
-            minOccurs=1,
-            maxOccurs=1,
-            type=type(True)
-        )
-
-        self.replica = self.addLiteralInput(
-            identifier="replica",
-            title="Replica",
-            abstract="If flag is set then search will include replicated datasets.",
-            default=False,
-            minOccurs=1,
-            maxOccurs=1,
-            type=type(True)
-        )
-
-        self.latest = self.addLiteralInput(
-            identifier="latest",
-            title="Latest",
-            abstract="If flag is set then search will include only latest datasets.",
-            default=True,
-            minOccurs=1,
-            maxOccurs=1,
-            type=type(True)
-        )
-
-        self.temporal = self.addLiteralInput(
-            identifier="temporal",
-            title="Temporal",
-            abstract="If flag is set then search will use temporal filter.",
-            default=False,
-            minOccurs=1,
-            maxOccurs=1,
-            type=type(True)
-        )
-
-        self.search_type = self.addLiteralInput(
-            identifier="search_type",
-            title="Search Type",
-            abstract="Search on Datasets, Files or Aggregations",
-            default='Dataset',
-            minOccurs=1,
-            maxOccurs=1,
-            type=type(''),
-            allowedValues=['Dataset', 'File', 'Aggregation']
-        )
-
-        self.constraints = self.addLiteralInput(
-            identifier="constraints",
-            title="Constraints",
-            abstract="Constraints as list of key/value pairs.\
-             Example: project:CORDEX, time_frequency:mon, variable:tas",
-            default="project:CORDEX, time_frequency:mon, variable:tas",
-            minOccurs=1,
-            maxOccurs=1,
-            type=type('')
-        )
-
-        self.query = self.addLiteralInput(
-            identifier="query",
-            title="Query",
-            abstract="Freetext query. For Example: temperatue",
-            default='*',
-            minOccurs=0,
-            maxOccurs=1,
-            type=type('')
-        )
-
-        self.start = self.addLiteralInput(
-            identifier="start",
-            title="Start",
-            abstract="Startime: 2000-01-11T12:00:00Z",
-            type=type(datetime(year=2000, month=1, day=1)),
-            minOccurs=0,
-            maxOccurs=1,
-        )
-
-        self.end = self.addLiteralInput(
-            identifier="end",
-            title="End",
-            abstract="Endtime: 2005-12-31T12:00:00Z",
-            type=type(datetime(year=2005, month=12, day=31)),
-            minOccurs=0,
-            maxOccurs=1,
-        )
-
-        self.limit = self.addLiteralInput(
-            identifier="limit",
-            title="Limit",
-            abstract="Maximum number of datasets in search result",
-            default=10,
-            minOccurs=1,
-            maxOccurs=1,
-            type=type(1),
-            allowedValues=[0, 1, 2, 5, 10, 20, 50, 100, 200]
-        )
-
-        self.offset = self.addLiteralInput(
-            identifier="offset",
-            title="Offset",
-            abstract="Start search of datasets at offset.",
-            default=0,
-            minOccurs=1,
-            maxOccurs=1,
-            type=type(1),
-        )
-
-        self.output = self.addComplexOutput(
-            identifier="output",
-            title="Search Result",
-            abstract="JSON document with search result",
-            formats=[{"mimeType": "application/json"}],
-            asReference=True,
-        )
-
-        self.summary = self.addComplexOutput(
-            identifier="summary",
-            title="Search Result Summary",
-            abstract="JSON document with search result summary",
-            formats=[{"mimeType": "application/json"}],
-            asReference=True,
-        )
-
-        self.facet_counts = self.addComplexOutput(
-            identifier="facet_counts",
-            title="Facet Counts",
-            abstract="JSON document with facet counts for contstaints.",
-            formats=[{"mimeType": "application/json"}],
-            asReference=True,
-        )
-
-    def execute(self):
+    def _handler(self, request, response):
         esgsearch = ESGSearch(
-            url=self.url.getValue(),
-            distrib=self.distrib.getValue(),
-            replica=self.replica.getValue(),
-            latest=self.replica.getValue(),
+            url=request.inputs['url'][0].data,
+            distrib=request.inputs['distrib'][0].data,
+            replica=request.inputs['replica'][0].data,
+            latest=request.inputs['latest'][0].data,
         )
 
+        constrains_str = request.inputs['constraints'][0].data.strip()
         constraints = []
-        for constrain in self.constraints.getValue().strip().split(','):
+        for constrain in constrains_str.split(','):
             key, value = constrain.split(':')
             constraints.append((key.strip(), value.strip()))
 
         (result, summary, facet_counts) = esgsearch.search(
             constraints=constraints,
-            query=self.query.getValue(),
-            start=self.start.getValue(),
-            end=self.end.getValue(),
-            search_type=self.search_type.getValue(),
-            limit=self.limit.getValue(),
-            offset=self.offset.getValue(),
-            temporal=self.temporal.getValue())
+            query=request.inputs['query'][0].data,
+            start=request.inputs['start'][0].data,
+            end=request.inputs['end'][0].data,
+            search_type=request.inputs['search_type'][0].data,
+            limit=request.inputs['limit'][0].data,
+            offset=srequest.inputs['offset'][0].data,
+            temporal=request.inputs['temporal'][0].data)
 
-        outfile = 'out.json'
-        with open(outfile, 'w') as fp:
+        with open('out.json', 'w') as fp:
             json.dump(obj=result, fp=fp, indent=4, sort_keys=True)
-            self.output.setValue(outfile)
+            response.outputs['output'].file = fp.name
 
-        outfile = 'summary.json'
-        with open(outfile, 'w') as fp:
+        with open('summary.json', 'w') as fp:
             json.dump(obj=summary, fp=fp, indent=4, sort_keys=True)
-            self.summary.setValue(outfile)
+            response.outputs['summary'].file = fp.name
 
-        outfile = 'counts.json'
-        with open(outfile, 'w') as fp:
+        with open('counts.json', 'w') as fp:
             json.dump(obj=facet_counts, fp=fp, indent=4, sort_keys=True)
-            self.facet_counts.setValue(outfile)
+            response.outputs['facet_counts'].file = fp.name
+        return response
