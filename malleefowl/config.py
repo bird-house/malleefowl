a5ba9b78a50ccc978ac9875ff7f4a7686a0fb9eb
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-12 11:11:37 +0100

    updated config to pywps 4.x

--- malleefowl/config.py
+++ malleefowl/config.py
@@ -1,25 +1,16 @@
 import os
-from pywps import config as wpsconfig
+from pywps import configuration as wpsconfig
 
 import logging
 logger = logging.getLogger(__name__)
 
 
-def getConfigValue(*args):
-    try:
-        value = wpsconfig.getConfigValue(*args)
-    except Exception:
-        value = None
-        logger.exception("Could not get config value for %s", args)
-    return value
-
-
 def wps_url():
-    return wpsconfig.getConfigValue("wps", "serveraddress")
+    return wpsconfig.get_config_value("server", "url")
 
 
 def cache_path():
-    mypath = getConfigValue("cache", "cache_path")
+    mypath = wpsconfig.get_config_value("cache", "cache_path")
     if mypath is None or len(mypath) == 0:
         mypath = os.path.join(os.sep, "tmp", "cache")
     if not os.path.exists(mypath):
@@ -33,7 +24,7 @@ def cache_path():
 def archive_root():
     archive_root = None
     try:
-        archive_root = wpsconfig.getConfigValue("malleefowl", "archive_root")
+        archive_root = wpsconfig.get_config_value("cache", "archive_root")
     except Exception:
         archive_root = os.environ.get('ESGF_ARCHIVE_ROOT')
         logger.warn("archive root not configured. Using environment variable ESGF_ARCHIVE_ROOT: %s", archive_root)
