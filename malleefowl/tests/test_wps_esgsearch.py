578053c8e9a95d95e28af6abb0982b0ff7b4cbfc
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-12 18:10:38 +0100

    enabled esgsearch tests

--- malleefowl/tests/test_wps_esgsearch.py
+++ malleefowl/tests/test_wps_esgsearch.py
@@ -22,68 +22,64 @@ def test_dataset():
     print resp.get_data()
     assert_response_success(resp)
 
+
+@pytest.mark.online
+def test_dataset_with_spaces():
+    client = client_for(Service(processes=[ESGSearchProcess()]))
+    datainputs = "url={};search_type={};limit={};offset={};constraints={}".format(
+        'http://esgf-data.dkrz.de/esg-search',
+        'Dataset', '10', '10',
+        ' project: CORDEX, time_frequency : mon,variable:tas,  experiment:historical  ')
+    resp = client.get(
+        service='WPS', request='Execute', version='1.0.0',
+        identifier='esgsearch',
+        datainputs=datainputs)
+    assert_response_success(resp)
+
+
+@pytest.mark.online
+def test_dataset_out_of_limit():
+    client = client_for(Service(processes=[ESGSearchProcess()]))
+    datainputs = "url={};search_type={};limit={};offset={};constraints={}".format(
+        'http://esgf-data.dkrz.de/esg-search',
+        'Dataset', '100', '99',
+        'project:CORDEX,time_frequency:mon,variable:tas,experiment:historical')
+    resp = client.get(
+        service='WPS', request='Execute', version='1.0.0',
+        identifier='esgsearch',
+        datainputs=datainputs)
+    assert_response_success(resp)
+
+
+@pytest.mark.online
+def test_dataset_out_of_offset():
+    client = client_for(Service(processes=[ESGSearchProcess()]))
+    datainputs = "url={};search_type={};limit={};offset={};constraints={}".format(
+        'http://esgf-data.dkrz.de/esg-search',
+        'Dataset', '1', '1000',
+        'project:CORDEX,time_frequency:mon,variable:tas,experiment:historical')
+    resp = client.get(
+        service='WPS', request='Execute', version='1.0.0',
+        identifier='esgsearch',
+        datainputs=datainputs)
+    assert_response_success(resp)
+
+
+@pytest.mark.online
+def test_dataset_latest():
+    client = client_for(Service(processes=[ESGSearchProcess()]))
+    datainputs = "url={};search_type={};limit={};offset={};constraints={};latest={}".format(
+        'http://esgf-data.dkrz.de/esg-search',
+        'Dataset', '100', '0',
+        'project:CORDEX,time_frequency:mon,variable:tas,experiment:historical',
+        'False')
+    resp = client.get(
+        service='WPS', request='Execute', version='1.0.0',
+        identifier='esgsearch',
+        datainputs=datainputs)
+    assert_response_success(resp)
+
 """
-    @pytest.mark.online
-    def test_dataset_with_spaces(self):
-        inputs = []
-        inputs.append(('search_type', 'Dataset'))
-        inputs.append(('limit', '10'))
-        inputs.append(('offset', '10'))
-        inputs.append(
-            ('constraints', ' project: CORDEX, time_frequency : mon,variable:tas,  experiment:historical  '))
-
-        output = [('output', True), ('summary', True)]
-        execution = self.wps.execute(identifier="esgsearch", inputs=inputs, output=output)
-        monitorExecution(execution, sleepSecs=1)
-
-        assert execution.status == 'ProcessSucceeded'
-
-    @pytest.mark.online
-    def test_dataset_out_of_limit(self):
-        inputs = []
-        inputs.append(('search_type', 'Dataset'))
-        inputs.append(('limit', '100'))
-        inputs.append(('offset', '99'))
-        inputs.append(
-            ('constraints', 'project:CORDEX,time_frequency:mon,variable:tas,experiment:historical'))
-
-        output = [('output', True), ('summary', True)]
-        execution = self.wps.execute(identifier="esgsearch", inputs=inputs, output=output)
-        monitorExecution(execution, sleepSecs=1)
-
-        assert execution.status == 'ProcessSucceeded'
-
-    @pytest.mark.online
-    def test_dataset_out_of_offset(self):
-        inputs = []
-        inputs.append(('search_type', 'Dataset'))
-        inputs.append(('limit', '1'))
-        inputs.append(('offset', '1000'))
-        inputs.append(
-            ('constraints', 'project:CORDEX,time_frequency:mon,variable:tas,experiment:historical'))
-
-        output = [('output', True), ('summary', True)]
-        execution = self.wps.execute(identifier="esgsearch", inputs=inputs, output=output)
-        monitorExecution(execution, sleepSecs=1)
-
-        assert execution.status == 'ProcessSucceeded'
-
-    @pytest.mark.online
-    def test_dataset_latest(self):
-        inputs = []
-        inputs.append(('search_type', 'Dataset'))
-        inputs.append(('limit', '100'))
-        inputs.append(('offset', '0'))
-        inputs.append(('latest', 'False'))
-        inputs.append(
-            ('constraints', 'project:CORDEX,time_frequency:mon,variable:tas,experiment:historical'))
-
-        output = [('output', True), ('summary', True)]
-        execution = self.wps.execute(identifier="esgsearch", inputs=inputs, output=output)
-        monitorExecution(execution, sleepSecs=1)
-
-        assert execution.status == 'ProcessSucceeded'
-
     @pytest.mark.online
     def test_dataset_facet_counts(self):
         inputs = []
