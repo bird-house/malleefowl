75a3e131d838d50fe14bdbe39cff488cd4e6aa4c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-12 11:54:25 +0100

    converted thredds process to pywps 4.x

--- malleefowl/processes/__init__.py
+++ malleefowl/processes/__init__.py
@@ -1,11 +1,13 @@
 from .wps_esgflogon import MyProxyLogon
 from .wps_download import Download
+from .wps_thredds import ThreddsDownload
 from .wps_workflow import DispelWorkflow
 from .wps_workflow import DummyProcess
 
 processes = [
     MyProxyLogon(),
     Download(),
+    ThreddsDownload(),
     DispelWorkflow(),
     DummyProcess(),
 ]
--- malleefowl/processes/wps_thredds.py
+++ malleefowl/processes/wps_thredds.py
@@ -1,47 +1,56 @@
-from pywps.Process import WPSProcess
+import json
+
+from pywps import Process
+from pywps import LiteralInput
+from pywps import ComplexInput
+from pywps import ComplexOutput
+from pywps import Format, FORMATS
+from pywps.app.Common import Metadata
+
 from malleefowl import download
 
 
-class ThreddsDownload(WPSProcess):
+class ThreddsDownload(Process):
     def __init__(self):
-        WPSProcess.__init__(
-            self,
+        inputs = [
+            LiteralInput('url', 'URL',
+                         data_type='string',
+                         abstract="URL of the catalog.",
+                         min_occurs=1,
+                         max_occurs=1,
+                         ),
+        ]
+        outputs = [
+            ComplexOutput('output', 'Downloaded files',
+                          abstract="JSON document with list of downloaded files with file url.",
+                          as_reference=True,
+                          supported_formats=[Format('application/json')]),
+        ]
+
+        super(ThreddsDownload, self).__init__(
+            self._handler,
             identifier="thredds_download",
             title="Download files from Thredds Catalog",
-            version="0.4",
+            version="0.5",
             abstract="Downloads files from Thredds Catalog and provides file list as JSON Document.",
-            statusSupported=True,
-            storeSupported=True,
+            metadata=[
+                Metadata('Birdhouse', 'http://bird-house.github.io/'),
+                Metadata('User Guide', 'http://malleefowl.readthedocs.io/en/latest/'),
+            ],
+            inputs=inputs,
+            outputs=outputs,
+            status_supported=True,
+            store_supported=True,
         )
 
-        self.url = self.addLiteralInput(
-            identifier="url",
-            title="URL",
-            abstract="URL of the catalog",
-            minOccurs=1,
-            maxOccurs=1,
-            type=type(''),
-        )
-
-        self.output = self.addComplexOutput(
-            identifier="output",
-            title="Downloaded files",
-            abstract="JSON document with list of downloaded files with file url.",
-            metadata=[],
-            formats=[{"mimeType": "application/json"}],
-            asReference=True,
-        )
-
-    def monitor(self, msg, percent_done):
-        self.status.set(msg, percent_done)
-
-    def execute(self):
+    def _handler(self, request, response):
+        def monitor(message, progress):
+            response.update_status(message, progress)
         files = download.download_files_from_thredds(
-            url=self.url.getValue(),
-            monitor=self.monitor)
+            url=request.inputs['url'].data,
+            monitor=monitor)
 
-        import json
-        outfile = 'out.json'
-        with open(outfile, 'w') as fp:
+        with open('out.json', 'w') as fp:
             json.dump(obj=files, fp=fp, indent=4, sort_keys=True)
-        self.output.setValue(outfile)
+            request.outputs['outputs'].file = fp.name
+        return response
