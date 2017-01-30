f5cf05ad57cf869986d3de0bc52b93443565c9b6
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-30 11:31:16 +0100

    removed unused openid_logon

--- malleefowl/esgf/logon.py
+++ malleefowl/esgf/logon.py
@@ -15,8 +15,6 @@ See also:
 
 import os
 import requests
-from requests.auth import HTTPBasicAuth
-from cookielib import MozillaCookieJar
 import re
 from lxml import etree
 from io import BytesIO
@@ -25,59 +23,6 @@ from dateutil import parser as date_parser
 
 import logging
 logger = logging.getLogger(__name__)
-
-
-def _consumer(provider, url):
-    consumer = provider
-    if url:
-        from urlparse import urlparse
-        consumer = urlparse(url).netloc
-    return consumer
-
-
-def _password(interactive, password):
-    if interactive:
-        if password is None:
-            from getpass import getpass
-            password = getpass('Enter password: ')
-    return password
-
-
-def openid_logon(openid, password=None, interactive=False, outdir=None, url=None):
-    """
-    Uses the OpenID logon at an ESGF identity provider to get the credentials (cookies)
-
-    TODO: move this code to esgf pyclient
-
-    :return: cookies file
-    """
-    (username, provider, port) = parse_openid(openid)
-    consumer = _consumer(provider, url)
-    password = _password(interactive, password)
-    outdir = outdir or os.path.curdir
-
-    url = 'https://{0}/esg-orp/j_spring_openid_security_check.htm'.format(consumer)
-    data = dict(openid_identifier='https://{0}/esgf-idp/openid/'.format(provider), rememberOpenid='on')
-    auth = HTTPBasicAuth(username, password)
-    headers = {'esgf-idea-agent-type': 'basic_auth'}
-
-    session = requests.Session()
-    cookies = os.path.join(outdir, 'cookies.txt')
-    session.cookies = MozillaCookieJar(cookies)
-    if not os.path.exists(cookies):
-        # Create a new cookies file and set our Session's cookies
-        logger.debug("setting cookies")
-        session.cookies.save()
-    else:
-        # Load saved cookies from the file and use them in a request
-        logger.debug("loading saved cookies")
-        session.cookies.load(ignore_discard=True)
-    response = session.post(url, auth=auth, data=data, headers=headers, verify=True)
-    logger.debug("openid logon: status=%s", response.status_code)
-    response.raise_for_status()
-    session.cookies.save(ignore_discard=True)
-
-    return cookies
 
 
 def myproxy_logon_with_openid(openid, password=None, interactive=False, outdir=None):
