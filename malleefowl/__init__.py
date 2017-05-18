a16b0ad0791d80b5b8e297da1f64e67effd3f052
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-05-18 17:21:05 +0200

    added application

--- malleefowl/__init__.py
+++ malleefowl/__init__.py
@@ -1,1 +1,11 @@
+import os
+from pywps.app.Service import Service
+
+from malleefowl.processes import processes
+
 __version__ = "0.6.4"
+
+
+def application(environ, start_response):
+    app = Service(processes, [os.environ.get('PYWPS_CFG')])
+    return app(environ, start_response)
